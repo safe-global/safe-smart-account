54312314a5b8f1ae8dfd0ab7d45359d9c3bc9793
Georgi87 <stefan.david.george@gmail.com>
2017-10-01 18:06:03 +0200

    remove duplicated files and update deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
 var GnosisSafeFactory = artifacts.require("./GnosisSafeFactory.sol");
 var GnosisSafeWithDescriptionsFactory = artifacts.require("./GnosisSafeWithDescriptionsFactory.sol");
+var DailyLimitExceptionFactory = artifacts.require("./exceptions/DailyLimitExceptionFactory.sol");
+var WhitelistExceptionFactory = artifacts.require("./exceptions/WhitelistExceptionFactory.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(GnosisSafeFactory);
     deployer.deploy(GnosisSafeWithDescriptionsFactory);
+    deployer.deploy(DailyLimitExceptionFactory);
+    deployer.deploy(WhitelistExceptionFactory);
 };
