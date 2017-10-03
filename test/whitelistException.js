be6554046876969c5352f4977d08bbd0af30c1ea
Georgi87 <stefan.david.george@gmail.com>
2017-10-03 17:36:29 +0200

    add tests for whitelist exception

--- test/whitelistException.js
+++ test/whitelistException.js
@@ -1,7 +1,6 @@
 const utils = require('./utils')
 
 const GnosisSafe = artifacts.require("./GnosisSafe.sol");
-const DailyLimitException = artifacts.require("./DailyLimitException.sol");
 const WhitelistException = artifacts.require("./WhitelistException.sol");
 
 contract('GnosisSafe', function(accounts) {
@@ -9,51 +8,11 @@ contract('GnosisSafe', function(accounts) {
     let gnosisSafe
     let data
     let transactionHash
-    let dailyLimitException
     let whitelistException
 
-    beforeEach(async () => {
-        
-    })
+    const CALL = 0
 
-    it('should create a new multisig contract with daily limit exception and withdraw daily limit', async () => {
-        // Create Gnosis Safe
-        gnosisSafe = await GnosisSafe.new([accounts[0], accounts[1]], 2)
-        // Create daily limit exception
-        dailyLimitException = await DailyLimitException.new(gnosisSafe.address, 200)
-        // Add exception to wallet
-        data = await gnosisSafe.contract.addException.getData(dailyLimitException.address)
-        transactionHash = await gnosisSafe.getTransactionHash(gnosisSafe.address, 0, data, 0, 0)
-        // Confirm transaction with account 0
-        await gnosisSafe.confirmTransaction(transactionHash, {from: accounts[0]})
-        // Confirm and execute transaction with account 1
-        utils.logGasUsage(
-            'confirmAndExecuteTransaction add exception',
-            await gnosisSafe.confirmAndExecuteTransaction(
-                gnosisSafe.address, 0, data, 0, 0, {from: accounts[1]}
-            )
-        )
-        // Deposit 1 eth
-        await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.toWei(1, 'ether')})
-        assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.toWei(1, 'ether'));
-        // Withdraw daily limit
-        utils.logGasUsage(
-            'executeException withdraw daily limit',
-            await gnosisSafe.executeException(
-                accounts[0], 100, 0, 0, dailyLimitException.address, {from: accounts[1]}
-            )
-        )
-        assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.toWei(1, 'ether') - 100);
-        utils.logGasUsage(
-            'executeException 2nd withdraw daily limit',
-            await gnosisSafe.executeException(
-                accounts[0], 100, 0, 0, dailyLimitException.address, {from: accounts[1]}
-            )
-        )
-        assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.toWei(1, 'ether') - 200);
-    })
-
-    it('should create a new multisig contract with whitelist exception and whitelisted withdraw', async () => {
+    it('should create a new Safe with whitelist exception and execute a withdraw transaction to a whitelisted account', async () => {
         // Create Gnosis Safe
         gnosisSafe = await GnosisSafe.new([accounts[0], accounts[1]], 2)
         // Create whitelist exception
@@ -64,12 +23,7 @@ contract('GnosisSafe', function(accounts) {
         // Confirm transaction with account 0
         await gnosisSafe.confirmTransaction(transactionHash, {from: accounts[0]})
         // Confirm and execute transaction with account 1
-        utils.logGasUsage(
-            'confirmAndExecuteTransaction add exception',
-            await gnosisSafe.confirmAndExecuteTransaction(
-                gnosisSafe.address, 0, data, 0, 0, {from: accounts[1]}
-            )
-        )
+        await gnosisSafe.confirmAndExecuteTransaction(gnosisSafe.address, 0, data, 0, 0, {from: accounts[1]})
         // Deposit 1 eth
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.toWei(1, 'ether')})
         assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.toWei(1, 'ether'));
@@ -81,5 +35,36 @@ contract('GnosisSafe', function(accounts) {
             )
         )
         assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.toWei(1, 'ether') - 300);
+    })
+
+    it('should create a new Safe with whitelist exception and add and remove an account from the whitelist', async () => {
+        // Create Gnosis Safe
+        gnosisSafe = await GnosisSafe.new([accounts[0], accounts[1]], 2)
+        // Create whitelist exception
+        whitelistException = await WhitelistException.new(gnosisSafe.address, [])
+        // Add exception to wallet
+        data = await gnosisSafe.contract.addException.getData(whitelistException.address)
+        transactionHash = await gnosisSafe.getTransactionHash(gnosisSafe.address, 0, data, 0, 0)
+        // Confirm transaction with account 0
+        await gnosisSafe.confirmTransaction(transactionHash, {from: accounts[0]})
+        // Confirm and execute transaction with account 1
+        await gnosisSafe.confirmAndExecuteTransaction(gnosisSafe.address, 0, data, 0, 0, {from: accounts[1]})
+        assert.equal(await whitelistException.isWhitelisted(accounts[3]), false);
+        // Add account 3 to whitelist
+        data = await whitelistException.contract.addToWhitelist.getData(accounts[3])
+        transactionHash = await gnosisSafe.getTransactionHash(whitelistException.address, 0, data, CALL, 0)
+        // Confirm transaction with account 0
+        await gnosisSafe.confirmTransaction(transactionHash, {from: accounts[0]})
+        // Confirm and execute transaction with account 1
+        await gnosisSafe.confirmAndExecuteTransaction(whitelistException.address, 0, data, CALL, 0, {from: accounts[1]})
+        assert.equal(await whitelistException.isWhitelisted(accounts[3]), true)
+        // Remove account 3 from whitelist
+        data = await whitelistException.contract.removeFromWhitelist.getData(accounts[3])
+        transactionHash = await gnosisSafe.getTransactionHash(whitelistException.address, 0, data, CALL, 0)
+        // Confirm transaction with account 0
+        await gnosisSafe.confirmTransaction(transactionHash, {from: accounts[0]})
+        // Confirm and execute transaction with account 1
+        await gnosisSafe.confirmAndExecuteTransaction(whitelistException.address, 0, data, CALL, 0, {from: accounts[1]})
+        assert.equal(await whitelistException.isWhitelisted(accounts[3]), false)
     })
 });
