faaa471dfef30fae62a1fa9e6813999109b8a7ef
Georgi87 <stefan.david.george@gmail.com>
2017-12-20 18:42:17 +0100

    add social recovery extension

---
+++ contracts/extensions/SocialRecoveryExtension.sol
@@ -0,0 +1,115 @@
+pragma solidity 0.4.19;
+import "../Extension.sol";
+import "../GnosisSafe.sol";
+
+
+/// @title Social Recovery Extension - Allows to replace an owner without if friends approve the replacement.
+/// @author Stefan George - <stefan@gnosis.pm>
+contract SocialRecoveryExtension is Extension {
+
+    string public constant NAME = "Social Recovery Extension";
+    string public constant VERSION = "0.0.1";
+    bytes4 public constant REPLACE_OWNER_FUNCTION_IDENTIFIER = hex"65098b86";
+
+    SocialRecoveryExtension masterCopy;
+    GnosisSafe public gnosisSafe;
+    uint8 public threshold;
+    address[] public friends;
+    mapping (address => bool) public isFriend;
+    mapping (bytes32 => bool) public isExecuted;
+    mapping (bytes32 => mapping (address => bool)) public isConfirmed;
+
+    modifier onlyGnosisSafe() {
+        require(msg.sender == address(gnosisSafe));
+        _;
+    }
+
+    modifier onlyFriend() {
+        require(isFriend[msg.sender]);
+        _;
+    }
+
+    function SocialRecoveryExtension(address[] _friends, uint8 _threshold)
+        public
+    {
+        setup(_friends, _threshold);
+    }
+
+    function setup(address[] _friends, uint8 _threshold)
+        public
+    {
+        require(address(gnosisSafe) == 0);
+        gnosisSafe = GnosisSafe(msg.sender);
+        require(_threshold <= _friends.length);
+        require(_threshold >= 2);
+        for (uint256 i = 0; i < _friends.length; i++) {
+            require(   _friends[i] != 0
+                    && !isFriend[_friends[i]]);
+            isFriend[_friends[i]] = true;
+        }
+        friends = _friends;
+        threshold = _threshold;
+    }
+
+    function changeMasterCopy(SocialRecoveryExtension _masterCopy)
+        public
+        onlyGnosisSafe
+    {
+        require(address(_masterCopy) != 0);
+        masterCopy = _masterCopy;
+    }
+
+    function confirmTransaction(bytes32 transactionHash)
+        public
+        onlyFriend
+    {
+        require(!isExecuted[transactionHash]);
+        require(!isConfirmed[transactionHash][msg.sender]);
+        isConfirmed[transactionHash][msg.sender] = true;
+    }
+
+    function isExecutable(address sender, address to, uint256 value, bytes data, GnosisSafe.Operation operation)
+        public
+        onlyGnosisSafe
+        returns (bool)
+    {
+        require(to == address(gnosisSafe));
+        require(value == 0);
+        require(operation == GnosisSafe.Operation.Call);
+        bytes4 functionIdentifier;
+        assembly {
+            functionIdentifier := mload(add(data, 32))
+        }
+        require(functionIdentifier == REPLACE_OWNER_FUNCTION_IDENTIFIER);
+        bytes32 transactionHash = getTransactionHash(to, value, data, operation);
+        if (   !isExecuted[transactionHash]
+            && isConfirmedByRequiredFriends(transactionHash)) {
+            isExecuted[transactionHash] = true;
+            return true;
+        }
+        return false;
+    }
+
+    function isConfirmedByRequiredFriends(bytes32 transactionHash)
+        public
+        view
+        returns (bool)
+    {
+        uint256 confirmationCount;
+        for (uint256 i = 0; i < friends.length; i++) {
+            if (isConfirmed[transactionHash][friends[i]])
+                confirmationCount++;
+            if (confirmationCount == threshold)
+                return true;
+        }
+        return false;
+    }
+
+    function getTransactionHash(address to, uint256 value, bytes data, GnosisSafe.Operation operation)
+        public
+        view
+        returns (bytes32)
+    {
+        return keccak256(to, value, data, operation);
+    }
+}
---
+++ test/socialRecoveryExtension.js
@@ -0,0 +1,51 @@
+const utils = require('./utils')
+
+const CreateAndAddExtension = artifacts.require("./libraries/CreateAndAddExtension.sol");
+const ProxyFactory = artifacts.require("./ProxyFactory.sol");
+const GnosisSafe = artifacts.require("./GnosisSafe.sol");
+const SocialRecoveryExtension = artifacts.require("./SocialRecoveryExtension.sol");
+
+contract('SocialRecoveryException', function(accounts) {
+
+    let gnosisSafe
+    let data
+    let transactionHash
+    let socialRecoveryException
+
+    const CALL = 0
+    const DELEGATECALL = 1
+
+    it('should create a new Safe with social recovery exception and allow to send a transaction apporved by friends', async () => {
+        // Create Master Copies
+        proxyFactory = await ProxyFactory.new()
+        createAndAddExtension = await CreateAndAddExtension.new()
+        gnosisSafeMasterCopy = await GnosisSafe.new([accounts[0], accounts[1]], 2, 0, 0)
+        socialRecoveryExtensionMasterCopy = await SocialRecoveryExtension.new([accounts[0], accounts[1]], 2)
+        // Create Gnosis Safe and Daily Limit Extension in one transactions
+        let extensionData = await socialRecoveryExtensionMasterCopy.contract.setup.getData([accounts[2], accounts[3]], 2)
+        let proxyFactoryData = await proxyFactory.contract.createProxy.getData(socialRecoveryExtensionMasterCopy.address, extensionData)
+        let createAndAddExtensionData = createAndAddExtension.contract.createAndAddExtension.getData(proxyFactory.address, proxyFactoryData)
+        gnosisSafeData = await gnosisSafeMasterCopy.contract.setup.getData([accounts[0], accounts[1]], 2, createAndAddExtension.address, createAndAddExtensionData)
+        gnosisSafe = utils.getParamFromTxEvent(
+            await proxyFactory.createProxy(gnosisSafeMasterCopy.address, gnosisSafeData),
+            'ProxyCreation', 'proxy', proxyFactory.address, GnosisSafe, 'create Gnosis Safe and Daily Limit Extension', 
+        )
+        extensions = await gnosisSafe.getExtensions()
+        socialRecoveryExtension = SocialRecoveryExtension.at(extensions[0])
+        assert.equal(await socialRecoveryExtension.gnosisSafe(), gnosisSafe.address)
+        // Replace owner
+        data = await gnosisSafe.contract.replaceOwner.getData(1, accounts[9])
+        // Confirm transaction to be executed without confirmations
+        transactionHash = await socialRecoveryExtension.getTransactionHash(gnosisSafe.address, 0, data, CALL)
+        await socialRecoveryExtension.confirmTransaction(transactionHash, {from: accounts[3]})
+        // Execution fails, because challenge period is not yet over
+        await utils.assertRejects(
+            gnosisSafe.executeExtension(gnosisSafe.address, 0, data, CALL, socialRecoveryExtension.address, {from: accounts[0]}),
+            "It was not confirmed by the required number of friends"
+        )
+        // Confirm with 2nd friend
+        await socialRecoveryExtension.confirmTransaction(transactionHash, {from: accounts[2]})
+        await gnosisSafe.executeExtension(gnosisSafe.address, 0, data, CALL, socialRecoveryExtension.address, {from: accounts[3]})
+        assert.equal(await gnosisSafe.isOwner(accounts[9]), true);
+    })
+});
