2c284a87bad1fc0eda3a3b28e9912a2776331d57
Georgi87 <stefan.david.george@gmail.com>
2018-01-04 20:07:26 +0100

    add commets to extension abstraction

--- contracts/Extension.sol
+++ contracts/Extension.sol
@@ -2,6 +2,8 @@ pragma solidity 0.4.19;
 import "./GnosisSafe.sol";
 
 
+/// @title Abstract Extension - Functions to be implemented by extensions.
+/// @author Stefan George - <stefan@gnosis.pm>
 contract Extension {
 
     function isExecutable(address sender, address to, uint256 value, bytes data, GnosisSafe.Operation operation) public returns (bool);
