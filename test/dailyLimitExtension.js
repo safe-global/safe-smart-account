9cacad5cb81b35e3679278a6d3cc73414cd2ed4d
Georgi87 <stefan.david.george@gmail.com>
2018-01-21 17:34:11 +0100

    refactor dailylimit extension tests

--- test/dailyLimitExtension.js
+++ test/dailyLimitExtension.js
@@ -1,46 +1,40 @@
 const utils = require('./utils')
 const solc = require('solc')
 
+const GnosisSafe = artifacts.require("./GnosisSafe.sol");
 const CreateAndAddExtension = artifacts.require("./libraries/CreateAndAddExtension.sol");
 const ProxyFactory = artifacts.require("./ProxyFactory.sol");
-const GnosisSafe = artifacts.require("./GnosisSafe.sol");
 const DailyLimitExtension = artifacts.require("./extensions/DailyLimitExtension.sol");
 
 
 contract('DailyLimitExtension', function(accounts) {
 
     let gnosisSafe
-    let lw
-    let data
-    let transactionHash
     let dailyLimitExtension
 
     const CALL = 0
-    const DELEGATECALL = 1
 
     beforeEach(async function () {
-        // Create lightwallet
-        lw = await utils.createLightwallet()
-    })
-
-    it('should create Gnosis Safe and Daily Limit Extension in one transaction', async () => {
         // Create Master Copies
-        proxyFactory = await ProxyFactory.new()
-        createAndAddExtension = await CreateAndAddExtension.new()
-        gnosisSafeMasterCopy = await GnosisSafe.new([accounts[0]], 1, 0, 0)
-        dailyLimitExtensionMasterCopy = await DailyLimitExtension.new([], [])
+        let proxyFactory = await ProxyFactory.new()
+        let createAndAddExtension = await CreateAndAddExtension.new()
+        let gnosisSafeMasterCopy = await GnosisSafe.new([accounts[0]], 1, 0, 0)
+        let dailyLimitExtensionMasterCopy = await DailyLimitExtension.new([], [])
         // Create Gnosis Safe and Daily Limit Extension in one transactions
         let extensionData = await dailyLimitExtensionMasterCopy.contract.setup.getData([0], [100])
         let proxyFactoryData = await proxyFactory.contract.createProxy.getData(dailyLimitExtensionMasterCopy.address, extensionData)
         let createAndAddExtensionData = createAndAddExtension.contract.createAndAddExtension.getData(proxyFactory.address, proxyFactoryData)
-        gnosisSafeData = await gnosisSafeMasterCopy.contract.setup.getData([accounts[0]], 1, createAndAddExtension.address, createAndAddExtensionData)
+        let gnosisSafeData = await gnosisSafeMasterCopy.contract.setup.getData([accounts[0]], 1, createAndAddExtension.address, createAndAddExtensionData)
         gnosisSafe = utils.getParamFromTxEvent(
             await proxyFactory.createProxy(gnosisSafeMasterCopy.address, gnosisSafeData),
             'ProxyCreation', 'proxy', proxyFactory.address, GnosisSafe, 'create Gnosis Safe and Daily Limit Extension', 
         )
-        extensions = await gnosisSafe.getExtensions()
+        let extensions = await gnosisSafe.getExtensions()
         dailyLimitExtension = DailyLimitExtension.at(extensions[0])
         assert.equal(await dailyLimitExtension.gnosisSafe(), gnosisSafe.address)
+    })
+
+    it('should create Gnosis Safe and Daily Limit Extension in one transaction', async () => {
         // Deposit 1 eth
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.toWei(1, 'ether')})
         assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.toWei(1, 'ether'));
@@ -68,43 +62,23 @@ contract('DailyLimitExtension', function(accounts) {
     })
 
     it('should change Daily limit', async () => {
-        // Create Master Copies
-        proxyFactory = await ProxyFactory.new()
-        createAndAddExtension = await CreateAndAddExtension.new()
-        gnosisSafeMasterCopy = await GnosisSafe.new([accounts[0]], 1, 0, 0)
-        dailyLimitExtensionMasterCopy = await DailyLimitExtension.new([], [])
-        // Create Gnosis Safe and Daily Limit Extension in one transactions
-        let extensionData = await dailyLimitExtensionMasterCopy.contract.setup.getData([0], [200])
-        let proxyFactoryData = await proxyFactory.contract.createProxy.getData(dailyLimitExtensionMasterCopy.address, extensionData)
-        let createAndAddExtensionData = createAndAddExtension.contract.createAndAddExtension.getData(proxyFactory.address, proxyFactoryData)
-        gnosisSafeData = await gnosisSafeMasterCopy.contract.setup.getData([lw.accounts[0], lw.accounts[1]], 2, createAndAddExtension.address, createAndAddExtensionData)
-        gnosisSafe = utils.getParamFromTxEvent(
-            await proxyFactory.createProxy(gnosisSafeMasterCopy.address, gnosisSafeData),
-            'ProxyCreation', 'proxy', proxyFactory.address, GnosisSafe, 'create Gnosis Safe and Daily Limit Extension', 
-        )
-        extensions = await gnosisSafe.getExtensions()
-        dailyLimitExtension = DailyLimitExtension.at(extensions[0])
-        assert.equal(await dailyLimitExtension.gnosisSafe(), gnosisSafe.address)
         // Change daily limit
         let dailyLimit = await dailyLimitExtension.dailyLimits(0)
-        console.log(dailyLimit[0].toNumber())
-        assert.equal(dailyLimit[0], 200);
-        data = await dailyLimitExtension.contract.changeDailyLimit.getData(0, 100)
-        nonce = await gnosisSafe.nonce()
-        transactionHash = await gnosisSafe.getTransactionHash(dailyLimitExtension.address, 0, data, CALL, nonce)
-        //Confirm transaction with signed messages
-        sigs = utils.signTransaction(lw, [lw.accounts[0], lw.accounts[1]], transactionHash)
+        assert.equal(dailyLimit[0], 100);
+        let data = await dailyLimitExtension.contract.changeDailyLimit.getData(0, 200)
+        let nonce = await gnosisSafe.nonce()
+        let transactionHash = await gnosisSafe.getTransactionHash(dailyLimitExtension.address, 0, data, CALL, nonce)
         utils.logGasUsage(
             'executeTransaction change daily limit',
             await gnosisSafe.executeTransaction(
-                dailyLimitExtension.address, 0, data, CALL, sigs.sigV, sigs.sigR, sigs.sigS, [], []
+                dailyLimitExtension.address, 0, data, CALL, [], [], [], [accounts[0]], [0]
             )
         )
         dailyLimit = await dailyLimitExtension.dailyLimits(0)
-        assert.equal(dailyLimit[0], 100);
+        assert.equal(dailyLimit[0], 200);
     })
 
-    it('should withdraw daily limit for an ERC20 token', async () => {
+    it.only('should withdraw daily limit for an ERC20 token', async () => {
         // Create fake token
         let source = `
         contract TestToken {
@@ -121,27 +95,15 @@ contract('DailyLimitExtension', function(accounts) {
         // Create test token contract
         let contractInterface = JSON.parse(output.contracts[':TestToken']['interface'])
         let contractBytecode = '0x' + output.contracts[':TestToken']['bytecode']
-        transactionHash = await web3.eth.sendTransaction({from: accounts[0], data: contractBytecode, gas: 4000000})
+        let transactionHash = await web3.eth.sendTransaction({from: accounts[0], data: contractBytecode, gas: 4000000})
         let receipt = web3.eth.getTransactionReceipt(transactionHash);
         const TestToken = web3.eth.contract(contractInterface)
         let testToken = TestToken.at(receipt.contractAddress)
-        // Create Master Copies
-        proxyFactory = await ProxyFactory.new()
-        createAndAddExtension = await CreateAndAddExtension.new()
-        gnosisSafeMasterCopy = await GnosisSafe.new([accounts[0]], 1, 0, 0)
-        dailyLimitExtensionMasterCopy = await DailyLimitExtension.new([], [])
-        // Create Gnosis Safe and Daily Limit Extension in one transactions
-        let extensionData = await dailyLimitExtensionMasterCopy.contract.setup.getData([testToken.address], [20])
-        let proxyFactoryData = await proxyFactory.contract.createProxy.getData(dailyLimitExtensionMasterCopy.address, extensionData)
-        let createAndAddExtensionData = createAndAddExtension.contract.createAndAddExtension.getData(proxyFactory.address, proxyFactoryData)
-        gnosisSafeData = await gnosisSafeMasterCopy.contract.setup.getData([accounts[0]], 1, createAndAddExtension.address, createAndAddExtensionData)
-        gnosisSafe = utils.getParamFromTxEvent(
-            await proxyFactory.createProxy(gnosisSafeMasterCopy.address, gnosisSafeData),
-            'ProxyCreation', 'proxy', proxyFactory.address, GnosisSafe, 'create Gnosis Safe and Daily Limit Extension', 
-        )
-        extensions = await gnosisSafe.getExtensions()
-        dailyLimitExtension = DailyLimitExtension.at(extensions[0])
-        assert.equal(await dailyLimitExtension.gnosisSafe(), gnosisSafe.address)
+        // Add test token to daily limit extension
+        let data = await dailyLimitExtension.contract.changeDailyLimit.getData(testToken.address, 20)
+        let nonce = await gnosisSafe.nonce()
+        transactionHash = await gnosisSafe.getTransactionHash(dailyLimitExtension.address, 0, data, CALL, nonce)
+        await gnosisSafe.executeTransaction(dailyLimitExtension.address, 0, data, CALL, [], [], [], [accounts[0]], [0])
         // Transfer 100 tokens to Safe
         assert.equal(await testToken.balances(gnosisSafe.address), 0);
         await testToken.transfer(gnosisSafe.address, 100, {from: accounts[0]})
