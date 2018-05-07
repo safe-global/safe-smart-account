c177d52e8c9799784381dc2f7d1af5d4d5fe53c1
Richard Meissner <msc.meissner@gmail.com>
2018-05-07 16:46:10 +0200

    Adjust migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,44 @@
 var ProxyFactory = artifacts.require("./ProxyFactory.sol");
-var GnosisSafe = artifacts.require("./GnosisSafe.sol");
-var DailyLimitExtension = artifacts.require("./DailyLimitExtension.sol");
-var SocialRecoveryExtension = artifacts.require("./SocialRecoveryExtension.sol");
-var WhitelistExtension = artifacts.require("./WhitelistExtension.sol");
-var CreateAndAddExtension = artifacts.require("./CreateAndAddExtension.sol");
+var GnosisSafePersonalEdition = artifacts.require("./GnosisSafePersonalEdition.sol");
+var GnosisSafeTeamEdition = artifacts.require("./GnosisSafeTeamEdition.sol");
+var GnosisSafeStateChannelEdition = artifacts.require("./GnosisSafeStateChannelEdition.sol");
+var DailyLimitModule = artifacts.require("./DailyLimitModule.sol");
+var DailyLimitModuleWithSignature = artifacts.require("./DailyLimitModuleWithSignature.sol");
+var SocialRecoveryModule = artifacts.require("./SocialRecoveryModule.sol");
+var WhitelistModule = artifacts.require("./WhitelistModule.sol");
+var CreateAndAddModule = artifacts.require("./CreateAndAddModule.sol");
 var MultiSend = artifacts.require("./MultiSend.sol");
 
 
 const notOwnedAddress = "0x0000000000000000000000000000000000000001"
 const notOwnedAddress2 = "0x0000000000000000000000000000000000000002"
 
+let initSafe = function (safe) {
+    safe.setup([notOwnedAddress], 1, 0, 0)
+    return safe
+}
+
 module.exports = function(deployer) {
     deployer.deploy(ProxyFactory);
-    deployer.deploy(GnosisSafe, [notOwnedAddress], 1, 0, 0);
-    deployer.deploy(DailyLimitExtension, [], []);
-    deployer.deploy(SocialRecoveryExtension, [notOwnedAddress, notOwnedAddress2], 2);
-    deployer.deploy(WhitelistExtension, []);
-    deployer.deploy(CreateAndAddExtension);
+    deployer.deploy(GnosisSafePersonalEdition).then(initSafe);
+    deployer.deploy(GnosisSafeTeamEdition).then(initSafe);
+    deployer.deploy(GnosisSafeStateChannelEdition).then(initSafe);
+    deployer.deploy(DailyLimitModule).then(function (module) {
+        module.setup([],[])
+        return module
+    });
+    deployer.deploy(DailyLimitModuleWithSignature).then(function (module) {
+        module.setup([],[])
+        return module
+    });
+    deployer.deploy(SocialRecoveryModule).then(function (module) {
+        module.setup([notOwnedAddress, notOwnedAddress2], 2)
+        return module
+    });
+    deployer.deploy(WhitelistModule).then(function (module) {
+        module.setup([])
+        return module
+    });
+    deployer.deploy(CreateAndAddModule);
     deployer.deploy(MultiSend);
 };
