b42b5f981ea674efd77fda0f982e629b16557c47
Richard Meissner <msc.meissner@gmail.com>
2018-05-28 12:23:08 +0200

    Add mnemonic logic for deployment

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const mnemonic = process.env.MNEMONIC
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +9,27 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4600000
+    },
+    rinkeby: {
+      provider: function() {
+                  return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/');
+                },
+      network_id: '4',
+      gas: 6700000,
+      gasPrice: 1000000000, // 1 Gwei
+    },
+    mainnet: {
+      provider: function() {
+                  return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io');
+                },
+      network_id: '1',
+      gas: 6700000,
+      gasPrice: 25000000000, // 25 Gwei
     }
-  }
+  },
+  solc: {
+    optimizer: {
+      enabled: false
+    },
+  },
 };
