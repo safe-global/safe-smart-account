35c50da76c529f611dcf2463194a1b6098cf87aa
Richard Meissner <msc.meissner@gmail.com>
2018-09-05 11:57:36 +0200

    Fix build

--- truffle.js
+++ truffle.js
@@ -1,7 +1,11 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
 const mnemonic = process.env.MNEMONIC
-const hdProvider = new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/')
+var hdProvider
+const getHDProvider = function() {
+  if (!hdProvider) hdProvider = new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/');
+  return hdProvider;
+}
 
 module.exports = {
   networks: {
@@ -12,13 +16,13 @@ module.exports = {
       gas: 6000000
     },
     rinkeby: {
-      provider: hdProvider,
+      provider: getHDProvider,
       network_id: '4',
       gas: 6700000,
-      gasPrice: 100000000000, // 1 Gwei
+      gasPrice: 1000000000, // 1 Gwei
     },
     mainnet: {
-      provider: hdProvider,
+      provider: getHDProvider,
       network_id: '1',
       gas: 6700000,
       gasPrice: 25000000000, // 25 Gwei
