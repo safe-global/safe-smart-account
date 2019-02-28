14931a00ccfa9dc21777346238471a3ddd975fd7
Richard Meissner <msc.meissner@gmail.com>
2019-02-28 12:26:31 +0100

    rename nonce to saltNonce

--- contracts/proxies/ProxyFactory.sol
+++ contracts/proxies/ProxyFactory.sol
@@ -37,13 +37,13 @@ contract ProxyFactory {
     /// @dev Allows to create new proxy contact and execute a message call to the new proxy within one transaction.
     /// @param _mastercopy Address of master copy.
     /// @param initializer Payload for message call sent to new proxy contract.
-    /// @param nonce Nonce that will be used to generate the salt to calculate the address of the new proxy contract.
-    function createProxyWithNonce(address _mastercopy, bytes memory initializer, uint256 nonce)
+    /// @param saltNonce Nonce that will be used to generate the salt to calculate the address of the new proxy contract.
+    function createProxyWithNonce(address _mastercopy, bytes memory initializer, uint256 saltNonce)
         public
         returns (Proxy proxy)
     {
         // If the initializer changes the proxy address should change too. Hashing the initializer data is cheaper than just concatinating it
-        bytes32 salt = keccak256(abi.encodePacked(keccak256(initializer), nonce));
+        bytes32 salt = keccak256(abi.encodePacked(keccak256(initializer), saltNonce));
         bytes memory deploymentData = abi.encodePacked(type(Proxy).creationCode, uint256(_mastercopy));
         // solium-disable-next-line security/no-inline-assembly
         assembly {
