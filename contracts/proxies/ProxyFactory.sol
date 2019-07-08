42fd01539f2c90a2f63d7d20249d531c37f4732d
Uxio Fuentefria <uxio@gnosis.pm>
2019-07-08 12:33:21 +0200

    Refactor CREATE2 repeated logic to one function

--- contracts/proxies/ProxyFactory.sol
+++ contracts/proxies/ProxyFactory.sol
@@ -34,12 +34,13 @@ contract ProxyFactory {
         return type(Proxy).creationCode;
     }
 
-    /// @dev Allows to create new proxy contact and execute a message call to the new proxy within one transaction.
+    /// @dev Allows to create new proxy contact using CREATE2 but it doesn't run the initializer.
+    ///      This method is only meant as an utility to be called from other methods
     /// @param _mastercopy Address of master copy.
     /// @param initializer Payload for message call sent to new proxy contract.
     /// @param saltNonce Nonce that will be used to generate the salt to calculate the address of the new proxy contract.
-    function createProxyWithNonce(address _mastercopy, bytes memory initializer, uint256 saltNonce)
-        public
+    function deployProxyWithNonce(address _mastercopy, bytes memory initializer, uint256 saltNonce)
+        internal
         returns (Proxy proxy)
     {
         // If the initializer changes the proxy address should change too. Hashing the initializer data is cheaper than just concatinating it
@@ -49,6 +50,17 @@ contract ProxyFactory {
         assembly {
             proxy := create2(0x0, add(0x20, deploymentData), mload(deploymentData), salt)
         }
+    }
+
+    /// @dev Allows to create new proxy contact and execute a message call to the new proxy within one transaction.
+    /// @param _mastercopy Address of master copy.
+    /// @param initializer Payload for message call sent to new proxy contract.
+    /// @param saltNonce Nonce that will be used to generate the salt to calculate the address of the new proxy contract.
+    function createProxyWithNonce(address _mastercopy, bytes memory initializer, uint256 saltNonce)
+        public
+        returns (Proxy proxy)
+    {
+        proxy = deployProxyWithNonce(_mastercopy, initializer, saltNonce);
         if (initializer.length > 0)
             // solium-disable-next-line security/no-inline-assembly
             assembly {
@@ -58,8 +70,8 @@ contract ProxyFactory {
     }
 
     /// @dev Allows to get the address for a new proxy contact created via `createProxyWithNonce`
-    ///      This method is only meant for address calculation purpose, therefore the response is returned with a revert
-    ///      When calling this method set `from` to the address of the proxy.
+    ///      This method is only meant for address calculation purpose when you use an initializer that would revert,
+    ///      therefore the response is returned with a revert. When calling this method set `from` to the address of the proxy factory.
     /// @param _mastercopy Address of master copy.
     /// @param initializer Payload for message call sent to new proxy contract.
     /// @param saltNonce Nonce that will be used to generate the salt to calculate the address of the new proxy contract.
@@ -67,15 +79,8 @@ contract ProxyFactory {
         external
         returns (Proxy proxy)
     {
-        // If the initializer changes the proxy address should change too. Hashing the initializer data is cheaper than just concatinating it
-        bytes32 salt = keccak256(abi.encodePacked(keccak256(initializer), saltNonce));
-        bytes memory deploymentData = abi.encodePacked(type(Proxy).creationCode, uint256(_mastercopy));
-        // solium-disable-next-line security/no-inline-assembly
-        assembly {
-            proxy := create2(0x0, add(0x20, deploymentData), mload(deploymentData), salt)
-        }
-
-	revert(string(abi.encodePacked(proxy)));
+        proxy = deployProxyWithNonce(_mastercopy, initializer, saltNonce);
+        revert(string(abi.encodePacked(proxy)));
     }
 
 }
