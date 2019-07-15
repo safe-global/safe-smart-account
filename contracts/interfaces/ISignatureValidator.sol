6528169a2d718cd4e0b993da697f9277bacdb385
Richard Meissner <richard@gnosis.io>
2019-07-15 19:26:41 +0200

    Closes #112: Use static call for EIP-1271 check

--- contracts/interfaces/ISignatureValidator.sol
+++ contracts/interfaces/ISignatureValidator.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.5.0;
 
-contract ISignatureValidator {
+contract ISignatureValidatorConstants {
     // bytes4(keccak256("isValidSignature(bytes,bytes)")
     bytes4 constant internal EIP1271_MAGIC_VALUE = 0x20c13b0b;
+}
+
+contract ISignatureValidator is ISignatureValidatorConstants {
 
     /**
     * @dev Should return whether the signature provided is valid for the provided data
@@ -12,10 +15,11 @@ contract ISignatureValidator {
     * MUST return the bytes4 magic value 0x20c13b0b when function passes.
     * MUST NOT modify state (using STATICCALL for solc < 0.5, view modifier for solc > 0.5)
     * MUST allow external calls
-    */ 
+    */
     function isValidSignature(
-        bytes calldata _data, 
-        bytes calldata _signature)
-        external 
+        bytes memory _data,
+        bytes memory _signature)
+        public
+        view
         returns (bytes4);
 }
