578205b3d22188f21d95339c1ad8b612554f8cea
Richard Meissner <richard@gnosis.io>
2019-10-15 11:15:06 +0200

    Add comments

--- contracts/proxies/Proxy.sol
+++ contracts/proxies/Proxy.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+/// @title IProxy - Helper interface to access masterCopy of the Proxy on-chain
+/// @author Richard Meissner - <richard@gnosis.io>
 interface IProxy {
     function masterCopy() external view returns (address);
 }
