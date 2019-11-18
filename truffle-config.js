a7bca70a8880c747343b51f27180e4c4dd3cffad
anon <sarah.anne.friend@gmail.com>
2019-11-18 13:34:44 -0500

    Truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,75 +1,35 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-require('dotenv').config()
-const package = require('./package')
-const mnemonic = process.env.MNEMONIC
-const token = process.env.INFURA_TOKEN
-
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+  solc: {
+    optimizer: {
+      enabled: false
     },
-    rinkeby: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/' + token)
-      },
-      network_id: '4',
-      gasPrice: 25000000000, // 25 Gwei
-    },
-    goerli: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://goerli.infura.io/v3/' + token)
-      },
-      network_id: '5',
-      gasPrice: 25000000000, // 25 Gwei
-    },
-    kovan: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + token)
-      },
-      network_id: '42',
-      gasPrice: 25000000000, // 25 Gwei
-    },
-    mainnet: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3/' + token)
-      },
-      network_id: '1',
-      gasPrice: 25000000000, // 25 Gwei
-    },
-    xdai: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://dai.poa.network')
-      },
-      network_id: '100',
-      gasPrice: 1000000000, // 1 Gwei
-    },
-    volta: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://volta-rpc.energyweb.org')
-      },
-      network_id: '73799',
-      gasPrice: 1
-    },
-    ewc: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://rpc.energyweb.org')
-      },
-      network_id: '246',
-      gasPrice: 1
+    settings: {
+      evmVersion: "petersburg"
     }
   },
-  compilers: {
-    solc: {
-      version: package.dependencies.solc,
-      settings: {
-        optimizer: {
-          enabled: false
-        },
-        evmVersion: "petersburg"
-      }
-    }
-  }
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
+    staging: {
+      host: 'api.joincircles.net',
+      port: 8545,
+      network_id: '*',
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
+  },
 };
+
