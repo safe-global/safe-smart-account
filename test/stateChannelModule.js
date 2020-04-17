f721a141c6c6832deeb8064bf472addcc9bba954
Richard Meissner <richard@gnosis.io>
2020-04-17 17:57:01 +0200

    Convert stateChannelModule test

--- test/stateChannelModule.js
+++ test/stateChannelModule.js
@@ -53,20 +53,22 @@ contract('StateChannelModule', function(accounts) {
         let stateChannelModuleMasterCopy = await StateChannelModule.new()
 
         // State channel module setup
-        let stateChannelSetupData = await stateChannelModuleMasterCopy.contract.setup.getData()
-        let stateChannelCreationData = await proxyFactory.contract.createProxy.getData(stateChannelModuleMasterCopy.address, stateChannelSetupData)
+        let stateChannelSetupData = await stateChannelModuleMasterCopy.contract.methods.setup().encodeABI()
+        let stateChannelCreationData = await proxyFactory.contract.methods.createProxy(stateChannelModuleMasterCopy.address, stateChannelSetupData).encodeABI()
 
         let modulesCreationData = utils.createAndAddModulesData([stateChannelCreationData])
-        let createAndAddModulesData = createAndAddModules.contract.createAndAddModules.getData(proxyFactory.address, modulesCreationData)
+        let createAndAddModulesData = createAndAddModules.contract.methods.createAndAddModules(proxyFactory.address, modulesCreationData).encodeABI()
 
         // Create Gnosis Safe
-        let gnosisSafeData = await gnosisSafeMasterCopy.contract.setup.getData([lw.accounts[0], lw.accounts[1], lw.accounts[2]], 2, createAndAddModules.address, createAndAddModulesData, 0, 0, 0, 0)
+        let gnosisSafeData = await gnosisSafeMasterCopy.contract.methods.setup(
+            [lw.accounts[0], lw.accounts[1], lw.accounts[2]], 2, createAndAddModules.address, createAndAddModulesData, utils.Address0, utils.Address0, 0, utils.Address0
+        ).encodeABI()
         gnosisSafe = await utils.getParamFromTxEvent(
             await proxyFactory.createProxy(gnosisSafeMasterCopy.address, gnosisSafeData),
             'ProxyCreation', 'proxy', proxyFactory.address, GnosisSafe, 'create Gnosis Safe Proxy',
         )
         let modules = await gnosisSafe.getModules()
-        stateChannelModule = StateChannelModule.at(modules[0])
+        stateChannelModule = await StateChannelModule.at(modules[0])
         assert.equal(await stateChannelModule.manager.call(), gnosisSafe.address)
     })
 
@@ -74,7 +76,7 @@ contract('StateChannelModule', function(accounts) {
         // Deposit 1 ETH + some spare money for execution 
         assert.equal(await web3.eth.getBalance(gnosisSafe.address), 0)
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.utils.toWei("1", 'ether')})
-        assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.utils.toWei("1", 'ether'))
+        assert.equal(await web3.eth.getBalance(gnosisSafe.address), web3.utils.toWei("1", 'ether'))
         // Should fail because there are not enough funds
         await executeTransaction('executeTransaction withdraw 2 ETH', [lw.accounts[0], lw.accounts[2]], accounts[0], web3.utils.toWei("2", 'ether'), "0x", CALL, true)
 
@@ -83,26 +85,26 @@ contract('StateChannelModule', function(accounts) {
 
         await executeTransaction('executeTransaction withdraw 0.5 ETH', [lw.accounts[0], lw.accounts[2]], accounts[0], web3.utils.toWei("0.5", 'ether'), "0x", CALL)
 
-        assert.equal(await web3.eth.getBalance(gnosisSafe.address).toNumber(), web3.utils.toWei("0", 'ether'))
+        assert.equal(await web3.eth.getBalance(gnosisSafe.address), web3.utils.toWei("0", 'ether'))
     })
 
     it('should add, remove and replace an owner and update the threshold', async () => {
         // Add owner and set threshold to 3
         assert.equal(await gnosisSafe.getThreshold(), 2)
-        let data = await gnosisSafe.contract.addOwnerWithThreshold.getData(accounts[1], 3)
+        let data = await gnosisSafe.contract.methods.addOwnerWithThreshold(accounts[1], 3).encodeABI()
         await executeTransaction('add owner and set threshold to 3', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL)
-        assert.deepEqual(await gnosisSafe.getOwners(), [accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[2]])
+        assert.deepEqual(await gnosisSafe.getOwners(), utils.formatAddresses([accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[2]]))
         assert.equal(await gnosisSafe.getThreshold(), 3)
 
         // Replace owner and keep threshold
-        data = await gnosisSafe.contract.swapOwner.getData(lw.accounts[1], lw.accounts[2], lw.accounts[3])
+        data = await gnosisSafe.contract.methods.swapOwner(lw.accounts[1], lw.accounts[2], lw.accounts[3]).encodeABI()
         await executeTransaction('replace owner', [lw.accounts[0], lw.accounts[1], lw.accounts[2]], gnosisSafe.address, 0, data, CALL)
-        assert.deepEqual(await gnosisSafe.getOwners(), [accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[3]])
+        assert.deepEqual(await gnosisSafe.getOwners(), utils.formatAddresses([accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[3]]))
 
         // Remove owner and reduce threshold to 2
-        data = await gnosisSafe.contract.removeOwner.getData(lw.accounts[1], lw.accounts[3], 2)
+        data = await gnosisSafe.contract.methods.removeOwner(lw.accounts[1], lw.accounts[3], 2).encodeABI()
         await executeTransaction('remove owner and reduce threshold to 2', [lw.accounts[0], lw.accounts[1], lw.accounts[3]], gnosisSafe.address, 0, data, CALL)
-        assert.deepEqual(await gnosisSafe.getOwners(), [accounts[1], lw.accounts[0], lw.accounts[1]])
+        assert.deepEqual(await gnosisSafe.getOwners(), utils.formatAddresses([accounts[1], lw.accounts[0], lw.accounts[1]]))
         assert.equal(await gnosisSafe.getThreshold(), 2)
     })
 })
