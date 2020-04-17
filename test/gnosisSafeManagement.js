6b33556c1970b3a90252e15108dbd0e1010b1fd0
Richard Meissner <richard@gnosis.io>
2020-04-17 16:44:35 +0200

    Convert gnosisSafeManagement test

--- test/gnosisSafeManagement.js
+++ test/gnosisSafeManagement.js
@@ -1,11 +1,8 @@
 const utils = require('./utils/general')
 const safeUtils = require('./utils/execution')
-const BigNumber = require('bignumber.js')
 
 const GnosisSafe = artifacts.require("./GnosisSafe.sol")
 const ProxyFactory = artifacts.require("./GnosisSafeProxyFactory.sol")
-const MockContract = artifacts.require('./MockContract.sol')
-const MockToken = artifacts.require('./Token.sol')
 
 contract('GnosisSafe owner and module management', function(accounts) {
 
@@ -15,7 +12,6 @@ contract('GnosisSafe owner and module management', function(accounts) {
     let executor = accounts[8]
 
     const CALL = 0
-    const CREATE = 2
 
     beforeEach(async function () {
         // Create lightwallet
@@ -24,44 +20,50 @@ contract('GnosisSafe owner and module management', function(accounts) {
         let proxyFactory = await ProxyFactory.new()
         gnosisSafeMasterCopy = await utils.deployContract("deploying Gnosis Safe Mastercopy", GnosisSafe)
         // Create Gnosis Safe
-        let gnosisSafeData = await gnosisSafeMasterCopy.contract.setup.getData([lw.accounts[0], lw.accounts[1], lw.accounts[2]], 2, 0, "0x", 0, 0, 0, 0)
+        let gnosisSafeData = await gnosisSafeMasterCopy.contract.methods.setup(
+            [lw.accounts[0], lw.accounts[1], lw.accounts[2]], 2, utils.Address0, "0x", utils.Address0, utils.Address0, 0, utils.Address0
+        ).encodeABI()
         gnosisSafe = await utils.getParamFromTxEvent(
             await proxyFactory.createProxy(gnosisSafeMasterCopy.address, gnosisSafeData),
             'ProxyCreation', 'proxy', proxyFactory.address, GnosisSafe, 'create Gnosis Safe Proxy',
         )
     })
 
+    const formatAddress = (address) => web3.utils.toChecksumAddress(address)
+
+    const formatAddresses = (addressArray) => addressArray.map((o) => web3.utils.toChecksumAddress(o))
+
     it('should add, remove and replace an owner and update the threshold and emit events', async () => {
         // Fund account for execution
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.utils.toWei("0.1", 'ether')})
 
-        let executorBalance = await web3.eth.getBalance(executor).toNumber()
+        let executorBalance = await web3.eth.getBalance(executor)
         // Add owner and set threshold to 3
         assert.equal(await gnosisSafe.getThreshold(), 2)
-        let data = await gnosisSafe.contract.addOwnerWithThreshold.getData(accounts[1], 3)
+        let data = await gnosisSafe.contract.methods.addOwnerWithThreshold(accounts[1], 3).encodeABI()
         let addTx = await safeUtils.executeTransaction(lw, gnosisSafe, 'add owner and set threshold to 3', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor)
-        assert.equal(utils.checkTxEvent(addTx, 'AddedOwner', gnosisSafe.address, true).args.owner, accounts[1])
+        assert.equal(utils.checkTxEvent(addTx, 'AddedOwner', gnosisSafe.address, true).args.owner, formatAddress(accounts[1]))
         assert.equal(utils.checkTxEvent(addTx, 'ChangedThreshold', gnosisSafe.address, true).args.threshold.toNumber(), 3)
-        assert.deepEqual(await gnosisSafe.getOwners(), [accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[2]])
+        assert.deepEqual(await gnosisSafe.getOwners(), formatAddresses([accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[2]]))
         assert.equal(await gnosisSafe.getThreshold(), 3)
 
         // Replace owner and keep threshold
-        data = await gnosisSafe.contract.swapOwner.getData(lw.accounts[1], lw.accounts[2], lw.accounts[3])
+        data = await gnosisSafe.contract.methods.swapOwner(lw.accounts[1], lw.accounts[2], lw.accounts[3]).encodeABI()
         let swapTx = await safeUtils.executeTransaction(lw, gnosisSafe, 'replace owner', [lw.accounts[0], lw.accounts[1], lw.accounts[2]], gnosisSafe.address, 0, data, CALL, executor)
-        assert.equal(utils.checkTxEvent(swapTx, 'RemovedOwner', gnosisSafe.address, true).args.owner, lw.accounts[2])
-        assert.equal(utils.checkTxEvent(swapTx, 'AddedOwner', gnosisSafe.address, true).args.owner, lw.accounts[3])
-        assert.deepEqual(await gnosisSafe.getOwners(), [accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[3]])
+        assert.equal(utils.checkTxEvent(swapTx, 'RemovedOwner', gnosisSafe.address, true).args.owner, formatAddress(lw.accounts[2]))
+        assert.equal(utils.checkTxEvent(swapTx, 'AddedOwner', gnosisSafe.address, true).args.owner, formatAddress(lw.accounts[3]))
+        assert.deepEqual(await gnosisSafe.getOwners(), formatAddresses([accounts[1], lw.accounts[0], lw.accounts[1], lw.accounts[3]]))
 
         // Remove owner and reduce threshold to 2
-        data = await gnosisSafe.contract.removeOwner.getData(lw.accounts[1], lw.accounts[3], 2)
+        data = await gnosisSafe.contract.methods.removeOwner(lw.accounts[1], lw.accounts[3], 2).encodeABI()
         let removeTx = await safeUtils.executeTransaction(lw, gnosisSafe, 'remove owner and reduce threshold to 2', [lw.accounts[0], lw.accounts[1], lw.accounts[3]], gnosisSafe.address, 0, data, CALL, executor)
-        assert.equal(utils.checkTxEvent(removeTx, 'RemovedOwner', gnosisSafe.address, true).args.owner, lw.accounts[3])
+        assert.equal(utils.checkTxEvent(removeTx, 'RemovedOwner', gnosisSafe.address, true).args.owner, formatAddress(lw.accounts[3]))
         assert.equal(utils.checkTxEvent(removeTx, 'ChangedThreshold', gnosisSafe.address, true).args.threshold.toNumber(), 2)
-        assert.deepEqual(await gnosisSafe.getOwners(), [accounts[1], lw.accounts[0], lw.accounts[1]])
+        assert.deepEqual(await gnosisSafe.getOwners(), formatAddresses([accounts[1], lw.accounts[0], lw.accounts[1]]))
         assert.equal(await gnosisSafe.getThreshold(), 2)
 
         let executorDiff = await web3.eth.getBalance(executor) - executorBalance
-        console.log("    Executor earned " + web3.utils.fromWei("executorDiff", 'ether') + " ETH")
+        console.log("    Executor earned " + web3.utils.fromWei(executorDiff.toString(), 'ether') + " ETH")
         assert.ok(executorDiff > 0)
     })
 
@@ -70,15 +72,15 @@ contract('GnosisSafe owner and module management', function(accounts) {
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.utils.toWei("0.1", 'ether')})
 
 	    // Check that the current address is pointing to the master copy
-        assert.equal(await web3.eth.getStorageAt(gnosisSafe.address, 0), gnosisSafeMasterCopy.address)
+        assert.equal(await web3.eth.getStorageAt(gnosisSafe.address, 0), gnosisSafeMasterCopy.address.toLowerCase())
 
         // We deploy a new master copy
         let newMasterCopy = await utils.deployContract("deploying Gnosis Safe Mastercopy", GnosisSafe)
 
-        let data = await gnosisSafe.contract.changeMasterCopy.getData(newMasterCopy.address)
+        let data = await gnosisSafe.contract.methods.changeMasterCopy(newMasterCopy.address).encodeABI()
         let updateTx = await safeUtils.executeTransaction(lw, gnosisSafe, 'update master copy', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor)
         assert.equal(utils.checkTxEvent(updateTx, 'ChangedMasterCopy', gnosisSafe.address, true).args.masterCopy, newMasterCopy.address)
-        assert.equal(await web3.eth.getStorageAt(gnosisSafe.address, 0), newMasterCopy.address)
+        assert.equal(await web3.eth.getStorageAt(gnosisSafe.address, 0), newMasterCopy.address.toLowerCase())
     })
 
 
@@ -88,45 +90,45 @@ contract('GnosisSafe owner and module management', function(accounts) {
         // Fund account for execution
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.utils.toWei("0.1", 'ether')})
 
-        let executorBalance = await web3.eth.getBalance(executor).toNumber()
+        let executorBalance = await web3.eth.getBalance(executor)
         // Check initial state
         assert.equal(await gnosisSafe.getThreshold(), 2)
-        assert.deepEqual(await gnosisSafe.getOwners(), [lw.accounts[0], lw.accounts[1], lw.accounts[2]])
+        assert.deepEqual(await gnosisSafe.getOwners(), formatAddresses([lw.accounts[0], lw.accounts[1], lw.accounts[2]]))
 
         // Invalid owner additions
-        let data = await gnosisSafe.contract.addOwnerWithThreshold.getData(zeroAcc, 3)
+        let data = await gnosisSafe.contract.methods.addOwnerWithThreshold(zeroAcc, 3).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'add zero account', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.addOwnerWithThreshold.getData(sentinel, 3)
+        data = await gnosisSafe.contract.methods.addOwnerWithThreshold(sentinel, 3).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'add sentinel', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
         // Invalid owner replacements
-        data = await gnosisSafe.contract.swapOwner.getData(sentinel, accounts[0], accounts[1])
+        data = await gnosisSafe.contract.methods.swapOwner(sentinel, accounts[0], accounts[1]).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'replace non-owner', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.swapOwner.getData(lw.accounts[2], sentinel, accounts[1])
+        data = await gnosisSafe.contract.methods.swapOwner(lw.accounts[2], sentinel, accounts[1]).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'replace sentinel', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.swapOwner.getData(accounts[1], zeroAcc, accounts[2])
+        data = await gnosisSafe.contract.methods.swapOwner(accounts[1], zeroAcc, accounts[2]).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'replace with zero account', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
         // Invalid owner removals
-        data = await gnosisSafe.contract.removeOwner.getData(sentinel, accounts[0], 1)
+        data = await gnosisSafe.contract.methods.removeOwner(sentinel, accounts[0], 1).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'remove non-owner', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.removeOwner.getData(lw.accounts[2], sentinel, 1)
+        data = await gnosisSafe.contract.methods.removeOwner(lw.accounts[2], sentinel, 1).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'remove sentinel', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.removeOwner.getData(accounts[1], zeroAcc, 1)
+        data = await gnosisSafe.contract.methods.removeOwner(accounts[1], zeroAcc, 1).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'remove with zero account', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
         let executorDiff = await web3.eth.getBalance(executor) - executorBalance
-        console.log("    Executor earned " + web3.utils.fromWei("executorDiff", 'ether') + " ETH")
+        console.log("    Executor earned " + web3.utils.fromWei(executorDiff.toString(), 'ether') + " ETH")
         assert.ok(executorDiff > 0)
 
         // Check that initial state still applies
         assert.equal(await gnosisSafe.getThreshold(), 2)
-        assert.deepEqual(await gnosisSafe.getOwners(), [lw.accounts[0], lw.accounts[1], lw.accounts[2]])
+        assert.deepEqual(await gnosisSafe.getOwners(), formatAddresses([lw.accounts[0], lw.accounts[1], lw.accounts[2]]))
     })
 
     it('should not be able to add/remove invalid modules', async () => {
@@ -136,39 +138,39 @@ contract('GnosisSafe owner and module management', function(accounts) {
         // Fund account for execution
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.utils.toWei("0.1", 'ether')})
 
-        let executorBalance = await web3.eth.getBalance(executor).toNumber()
+        let executorBalance = await web3.eth.getBalance(executor)
 
         // Add random account as module
         let randomModule = accounts[6]
-        let data = await gnosisSafe.contract.enableModule.getData(randomModule)
+        let data = await gnosisSafe.contract.methods.enableModule(randomModule).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'add random module', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor)
 
         // Check initial state
-        assert.deepEqual(await gnosisSafe.getModules(), [randomModule])
+        assert.deepEqual(await gnosisSafe.getModules(), formatAddresses([randomModule]))
 
         // Invalid module additions
-        data = await gnosisSafe.contract.enableModule.getData(zeroAcc)
+        data = await gnosisSafe.contract.methods.enableModule(zeroAcc).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'add zero account', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.enableModule.getData(sentinel)
+        data = await gnosisSafe.contract.methods.enableModule(sentinel).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'add sentinel', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
         // Invalid module removals
-        data = await gnosisSafe.contract.disableModule.getData(sentinel, accounts[0])
+        data = await gnosisSafe.contract.methods.disableModule(sentinel, accounts[0]).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'remove non-module', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.disableModule.getData(randomModule, sentinel)
+        data = await gnosisSafe.contract.methods.disableModule(randomModule, sentinel).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'remove sentinel', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
-        data = await gnosisSafe.contract.disableModule.getData(accounts[1], zeroAcc)
+        data = await gnosisSafe.contract.methods.disableModule(accounts[1], zeroAcc).encodeABI()
         await safeUtils.executeTransaction(lw, gnosisSafe, 'remove with zero account', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor, { fails: true})
 
         let executorDiff = await web3.eth.getBalance(executor) - executorBalance
-        console.log("    Executor earned " + web3.utils.fromWei("executorDiff", 'ether') + " ETH")
+        console.log("    Executor earned " + web3.utils.fromWei(executorDiff.toString(), 'ether') + " ETH")
         assert.ok(executorDiff > 0)
 
         // Check that initial state still applies
-        assert.deepEqual(await gnosisSafe.getModules(), [accounts[6]])
+        assert.deepEqual(await gnosisSafe.getModules(), formatAddresses([accounts[6]]))
     })
 
     it('should emit events for modules', async () => {
@@ -177,23 +179,23 @@ contract('GnosisSafe owner and module management', function(accounts) {
         // Fund account for execution
         await web3.eth.sendTransaction({from: accounts[0], to: gnosisSafe.address, value: web3.utils.toWei("0.1", 'ether')})
 
-        let executorBalance = await web3.eth.getBalance(executor).toNumber()
+        let executorBalance = await web3.eth.getBalance(executor)
 
         // Add random account as module
         let randomModule = accounts[6]
-        let data = await gnosisSafe.contract.enableModule.getData(randomModule)
+        let data = await gnosisSafe.contract.methods.enableModule(randomModule).encodeABI()
         let enableTx = await safeUtils.executeTransaction(lw, gnosisSafe, 'enable random module', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor)
-        assert.equal(utils.checkTxEvent(enableTx, 'EnabledModule', gnosisSafe.address, true).args.module, randomModule)
+        assert.equal(utils.checkTxEvent(enableTx, 'EnabledModule', gnosisSafe.address, true).args.module, formatAddress(randomModule))
 
         // Check state
-        assert.deepEqual(await gnosisSafe.getModules(), [randomModule])
+        assert.deepEqual(await gnosisSafe.getModules(), formatAddresses([randomModule]))
 
-        data = await gnosisSafe.contract.disableModule.getData(sentinel, randomModule)
+        data = await gnosisSafe.contract.methods.disableModule(sentinel, randomModule).encodeABI()
         let disableTx = await safeUtils.executeTransaction(lw, gnosisSafe, 'disable random module', [lw.accounts[0], lw.accounts[1]], gnosisSafe.address, 0, data, CALL, executor)
-        assert.equal(utils.checkTxEvent(disableTx, 'DisabledModule', gnosisSafe.address, true).args.module, randomModule)
+        assert.equal(utils.checkTxEvent(disableTx, 'DisabledModule', gnosisSafe.address, true).args.module, formatAddress(randomModule))
 
         let executorDiff = await web3.eth.getBalance(executor) - executorBalance
-        console.log("    Executor earned " + web3.utils.fromWei("executorDiff", 'ether') + " ETH")
+        console.log("    Executor earned " + web3.utils.fromWei(executorDiff.toString(), 'ether') + " ETH")
         assert.ok(executorDiff > 0)
 
         // Check final state
@@ -201,8 +203,9 @@ contract('GnosisSafe owner and module management', function(accounts) {
     })
 
     it('sentinels should not be owners or modules', async () => {
+        let sentinel = "0x0000000000000000000000000000000000000001"
 
-        assert.equal(await gnosisSafe.isOwner("0x1"), false)
+        assert.equal(await gnosisSafe.isOwner(sentinel), false)
 
         let sig = "0x" + "0000000000000000000000000000000000000000000000000000000000000001" + "0000000000000000000000000000000000000000000000000000000000000000" + "01"
         await utils.assertRejects(
