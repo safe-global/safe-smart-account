58a9699f6ae40148b28fb4ff3df59397a57acccc
anon <sarah.anne.friend@gmail.com>
2020-07-02 06:30:48 -0400

    Switch hdwallet provide

---
+++ truffle.js
@@ -0,0 +1,56 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
+require('dotenv').config()
+
+const mnemonic = process.env.MNEMONIC
+const token = process.env.INFURA_TOKEN
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/' + token)
+      },
+      network_id: '4',
+      gasPrice: 25000000000, // 25 Gwei
+    },
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://goerli.infura.io/v3/' + token)
+      },
+      network_id: '5',
+      gasPrice: 25000000000, // 25 Gwei
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + token)
+      },
+      network_id: '42',
+      gasPrice: 25000000000, // 25 Gwei
+    },
+    xdai: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://dai.poa.network')
+      },
+      network_id: '100',
+      gasPrice: 1000000000, // 25 Gwei
+    },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3/' + token)
+      },
+      network_id: '1',
+      gasPrice: 25000000000, // 25 Gwei
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: false
+    }
+  },
+};
