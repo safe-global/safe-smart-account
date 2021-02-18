d0fbb97cc1dcc77c039e89d3b37983abb360a3e0
Richard Meissner <msc.meissner@gmail.com>
2021-02-18 19:33:37 +0100

    Exclude test contracts from coverage

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,7 @@
 module.exports = {
-    skipFiles: ['test/Imports.sol', 'test/TestExecutor.sol'],
+    skipFiles: [
+        'test/*',
+    ],
     mocha: {
         grep: "@skip-on-coverage", // Find everything with this tag
         invert: true               // Run the grep's inverse set.
--- contracts/mocks/Token.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity >=0.6.0 <0.7.0;
-import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
-interface Token {
-	function transfer(address _to, uint value) external returns (bool);
-}
