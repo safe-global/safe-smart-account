d86d026c5b8e7fa086e832e28c11904075139abb
Richard Meissner <msc.meissner@gmail.com>
2021-02-21 23:29:46 +0100

    Add all supported network configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,7 @@ if (PK) {
   };
 }
 
-if (["rinkeby", "mainnet"].includes(argv.network) && INFURA_KEY === undefined) {
+if (["mainnet", "rinkeby", "kovan", "goerli"].includes(argv.network) && INFURA_KEY === undefined) {
   throw new Error(
     `Could not find Infura key in env, unable to connect to network ${argv.network}`,
   );
@@ -62,13 +62,29 @@ export default {
       ...sharedNetworkConfig,
       url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
     },
+    xdai: {
+      ...sharedNetworkConfig,
+      url: "https://xdai.poanetwork.dev",
+    },
+    ewc: {
+      ...sharedNetworkConfig,
+      url: `https://rpc.energyweb.org`,
+    },
     rinkeby: {
       ...sharedNetworkConfig,
       url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
     },
-    xdai: {
+    goerli: {
       ...sharedNetworkConfig,
-      url: "https://xdai.poanetwork.dev",
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+    },
+    kovan: {
+      ...sharedNetworkConfig,
+      url: `https://kovan.infura.io/v3/${INFURA_KEY}`,
+    },
+    volta: {
+      ...sharedNetworkConfig,
+      url: `https://volta-rpc.energyweb.org`,
     },
   },
   namedAccounts: {
