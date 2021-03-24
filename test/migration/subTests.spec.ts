20bc813e81c4f8a84dabed2fd446115b214e0a52
Richard Meissner <msc.meissner@gmail.com>
2021-03-24 18:41:30 +0100

    Add test for module management

--- test/migration/subTests.spec.ts
+++ test/migration/subTests.spec.ts
@@ -3,6 +3,7 @@ import { Contract } from "@ethersproject/contracts"
 import { parseEther } from "@ethersproject/units"
 import { expect } from "chai";
 import hre, { ethers, waffle } from "hardhat";
+import { AddressOne } from "../utils/constants";
 import { buildSafeTransaction, executeContractCallWithSigners, executeTx, executeTxWithSigners } from "../utils/execution"
 
 interface TestSetup {
@@ -52,6 +53,25 @@ export const verificationTests = (setupTests: () => Promise<TestSetup>) => {
             await expect(await migratedSafe.isOwner(user1.address)).to.be.true
             await expect(await migratedSafe.isOwner(user2.address)).to.be.true
             await expect(await migratedSafe.isOwner(user3.address)).to.be.true
+        })
+    })
+
+    describe("enableModule", async () => {
+        it('should enabled module and be able to use it', async () => {
+            const { migratedSafe, mock } = await setupTests()
+
+            await expect(
+                executeContractCallWithSigners(migratedSafe, migratedSafe, "enableModule", [user2.address], [user1])
+            ).to.emit(migratedSafe, "EnabledModule").withArgs(user2.address)
+
+            await expect(await migratedSafe.isModuleEnabled(user2.address)).to.be.true
+            await expect(await migratedSafe.getModulesPaginated(AddressOne, 10)).to.be.deep.equal([[user2.address], AddressOne])
+
+            const user2Safe = migratedSafe.connect(user2)
+            await expect(
+                user2Safe.execTransactionFromModule(mock.address, 0, "0xbaddad", 0)
+            ).to.emit(migratedSafe, "ExecutionFromModuleSuccess").withArgs(user2.address)
+            expect(await mock.callStatic.invocationCountForCalldata("0xbaddad")).to.be.deep.equals(BigNumber.from(1));
         })
     })
 }
