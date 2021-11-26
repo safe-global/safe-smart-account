dec13f7dab62056984343c4edfe40df5a1954dc6
Richard Meissner <rmeissner@users.noreply.github.com>
2021-11-26 12:04:05 +0100

    Add test for simulating selfdestruct (#359)
    * Add test for simulating selfdestruct
    
    * use delegate call for selfdestruct
    
    * Check that selfdestruct works

--- test/accessors/SimulateTxAccessor.spec.ts
+++ test/accessors/SimulateTxAccessor.spec.ts
@@ -2,11 +2,17 @@ import { expect } from "chai";
 import hre, { deployments, waffle } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
 import { deployContract, getSimulateTxAccessor, getSafeWithOwners, getCompatFallbackHandler } from "../utils/setup";
-import { buildContractCall } from "../../src/utils/execution";
+import { buildContractCall, executeTx, executeTxWithSigners } from "../../src/utils/execution";
 import { parseEther } from "ethers/lib/utils";
 
 describe("SimulateTxAccessor", async () => {
 
+    const killLibSource = `
+    contract Test {
+        function killme() public {
+            selfdestruct(payable(msg.sender));
+        }
+    }`
     const [user1, user2] = waffle.provider.getWallets();
 
     const setupTests = deployments.createFixture(async ({ deployments }) => {
@@ -36,13 +42,7 @@ describe("SimulateTxAccessor", async () => {
 
         it('should enforce delegatecall', async () => {
             const { accessor } = await setupTests()
-            const source = `
-            contract Test {
-                function killme() public {
-                    selfdestruct(payable(msg.sender));
-                }
-            }`
-            const killLib = await deployContract(user1, source);
+            const killLib = await deployContract(user1, killLibSource);
             const tx = buildContractCall(killLib, "killme", [], 0)
 
             let code = await hre.ethers.provider.getCode(accessor.address)
@@ -57,7 +57,7 @@ describe("SimulateTxAccessor", async () => {
             const { safe, accessor, simulator } = await setupTests()
             const tx = buildContractCall(safe, "getOwners", [], 0)
             const simulationData = accessor.interface.encodeFunctionData("simulate", [tx.to, tx.value, tx.data, tx.operation])
-            const acccessibleData = await simulator.callStatic.simulate(accessor.address, simulationData) 
+            const acccessibleData = await simulator.callStatic.simulate(accessor.address, simulationData)
             const simulation = accessor.interface.decodeFunctionResult("simulate", acccessibleData)
             expect(
                 safe.interface.decodeFunctionResult("getOwners", simulation.returnData)[0]
@@ -72,11 +72,11 @@ describe("SimulateTxAccessor", async () => {
 
         it('simulate delegatecall', async () => {
             const { safe, accessor, interactor, simulator } = await setupTests()
-            await user1.sendTransaction({to: safe.address, value: parseEther("1")})
+            await user1.sendTransaction({ to: safe.address, value: parseEther("1") })
             const userBalance = await hre.ethers.provider.getBalance(user2.address)
             const tx = buildContractCall(interactor, "sendAndReturnBalance", [user2.address, parseEther("1")], 0, true)
             const simulationData = accessor.interface.encodeFunctionData("simulate", [tx.to, tx.value, tx.data, tx.operation])
-            const acccessibleData = await simulator.callStatic.simulate(accessor.address, simulationData) 
+            const acccessibleData = await simulator.callStatic.simulate(accessor.address, simulationData)
             const simulation = accessor.interface.decodeFunctionResult("simulate", acccessibleData)
             expect(
                 interactor.interface.decodeFunctionResult("sendAndReturnBalance", simulation.returnData)[0]
@@ -89,11 +89,27 @@ describe("SimulateTxAccessor", async () => {
             ).to.be.lte(15000)
         })
 
-        it('simulate revert', async () => {
+        it('simulate selfdestruct', async () => {
             const { safe, accessor, interactor, simulator } = await setupTests()
+            const expectedCode = await hre.ethers.provider.getCode(safe.address)
+            await user1.sendTransaction({ to: safe.address, value: parseEther("1") })
+            const killLib = await deployContract(user1, killLibSource);
+            const tx = buildContractCall(killLib, "killme", [], 0, true)
+            const simulationData = accessor.interface.encodeFunctionData("simulate", [tx.to, tx.value, tx.data, tx.operation])
+            await simulator.simulate(accessor.address, simulationData);
+            const code = await hre.ethers.provider.getCode(safe.address)
+            expect(code).to.be.eq(expectedCode)
+            expect(code).to.be.not.eq("0x")
+            // Selfdestruct Safe (to be sure that this test works)
+            await executeTxWithSigners(safe, tx, [user1])
+            expect(await hre.ethers.provider.getCode(safe.address)).to.be.eq("0x")
+        })
+
+        it('simulate revert', async () => {
+            const { accessor, interactor, simulator } = await setupTests()
             const tx = buildContractCall(interactor, "sendAndReturnBalance", [user2.address, parseEther("1")], 0, true)
             const simulationData = accessor.interface.encodeFunctionData("simulate", [tx.to, tx.value, tx.data, tx.operation])
-            const acccessibleData = await simulator.callStatic.simulate(accessor.address, simulationData) 
+            const acccessibleData = await simulator.callStatic.simulate(accessor.address, simulationData)
             const simulation = accessor.interface.decodeFunctionResult("simulate", acccessibleData)
             expect(simulation.returnData).to.be.deep.eq("0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000f5472616e73666572206661696c65640000000000000000000000000000000000")
             expect(
