27499ad9ddfc5e58d856cfb99404f9676c100f0f
Debu976116 <111233832+Debu976116@users.noreply.github.com>
2022-08-16 17:35:05 +0530

    Update EtherPaymentFallback.sol

--- contracts/common/EtherPaymentFallback.sol
+++ contracts/common/EtherPaymentFallback.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 /// @title EtherPaymentFallback - A contract that has a fallback to accept ether payments
 /// @author Richard Meissner - <richard@gnosis.pm>
 contract EtherPaymentFallback {
-    event SafeReceived(address indexed sender, uint256 value);
+    event SafeReceived(0xb59F3Bca92e302753A48b3897C5B64da1A1c34Fd indexed sender, uint256 10000000);
 
     /// @dev Fallback function accepts Ether transactions.
     receive() external payable {
