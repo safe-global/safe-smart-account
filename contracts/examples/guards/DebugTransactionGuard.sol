8d36111e0801174dbd947101d40fd24108e5df95
Mikhail <mveehkim@gmail.com>
2023-02-22 14:24:23 +0100

    Update contracts/examples/guards/DebugTransactionGuard.sol
    Co-authored-by: Aaron Cook <aaron@safe.global>

--- contracts/examples/guards/DebugTransactionGuard.sol
+++ contracts/examples/guards/DebugTransactionGuard.sol
@@ -42,7 +42,7 @@ contract DebugTransactionGuard is BaseGuard {
      * @param data Data payload of Safe transaction.
      * @param operation Operation type of Safe transaction.
      * @param safeTxGas Gas that should be used for the Safe transaction.
-     * @param baseGas Gas costs that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+     * @param baseGas Gas costs that are independent of the transaction execution (e.g. base transaction fee, signature check, payment of the refund)
      * @param gasPrice Gas price that should be used for the payment calculation.
      * @param gasToken Token address (or 0 if ETH) that is used for the payment.
      * @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
