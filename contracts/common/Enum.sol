9ec610d613b55b98f74d707282ab778f717d471a
Mikhail <mveehkim@gmail.com>
2023-02-22 13:23:52 +0100

    Docs: Improve documentation of `common/` contracts  (#510)
    * document common contracts
    
    * use native currency instead of ether

--- contracts/common/Enum.sol
+++ contracts/common/Enum.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.7.0 <0.9.0;
 
-/// @title Enum - Collection of enums
-/// @author Richard Meissner - <richard@gnosis.pm>
+/**
+ * @title Enum - Collection of enums used in Safe contracts.
+ * @author Richard Meissner - @rmeissner
+ */
 contract Enum {
     enum Operation {
         Call,
--- contracts/common/EtherPaymentFallback.sol
+++ contracts/common/EtherPaymentFallback.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.7.0 <0.9.0;
 
-/// @title EtherPaymentFallback - A contract that has a fallback to accept ether payments
-/// @author Richard Meissner - <richard@gnosis.pm>
+/**
+ * @title EtherPaymentFallback - A contract that has a fallback to accept native currency payments.
+ * @author Richard Meissner - @rmeissner
+ */
 contract EtherPaymentFallback {
     event SafeReceived(address indexed sender, uint256 value);
 
-    /// @dev Fallback function accepts Ether transactions.
+    /**
+     * @notice Receive function accepts native currency transactions.
+     * @dev Emits an event with sender and received value.
+     */
     receive() external payable {
         emit SafeReceived(msg.sender, msg.value);
     }
