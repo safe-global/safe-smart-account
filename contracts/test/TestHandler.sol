9a5eeff76ea539605bab4212e088ef0cd5f2eb77
Mikhail <mveehkim@gmail.com>
2023-02-23 15:55:48 +0100

    document test contracts

--- contracts/test/TestHandler.sol
+++ contracts/test/TestHandler.sol
@@ -3,7 +3,15 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "../handler/HandlerContext.sol";
 
+/**
+ * @title TestHandler - A test FallbackHandler contract
+ */
 contract TestHandler is HandlerContext {
+    /**
+     * @notice Returns the sender and manager address provided by the HandlerContext
+     * @return sender The sender address
+     * @return manager The manager address
+     */
     function dudududu() external view returns (address sender, address manager) {
         return (_msgSender(), _manager());
     }
