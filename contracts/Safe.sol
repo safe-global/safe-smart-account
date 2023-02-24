96a4e280876c33c53a09b5ef6ee78201a101ff58
Mikhail <mveehkim@gmail.com>
2023-02-24 10:31:45 +0100

    Update version to 1.4.0 in the solidity source (#521)
    * Update version to 1.4.0 in the solidity source
    
    * fix migration tests

--- contracts/Safe.sol
+++ contracts/Safe.sol
@@ -44,7 +44,7 @@ contract Safe is
 {
     using SafeMath for uint256;
 
-    string public constant VERSION = "1.3.0";
+    string public constant VERSION = "1.4.0";
 
     // keccak256(
     //     "EIP712Domain(uint256 chainId,address verifyingContract)"
