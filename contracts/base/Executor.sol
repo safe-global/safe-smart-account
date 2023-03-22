06cebbc7b1eab414a9daa4913b6699b7b1308561
Mikhail <mveehkim@gmail.com>
2023-03-22 10:25:13 +0100

    Update contracts/base/Executor.sol
    Co-authored-by: Aaron Cook <aaron@safe.global>

--- contracts/base/Executor.sol
+++ contracts/base/Executor.sol
@@ -9,8 +9,8 @@ import "../common/Enum.sol";
 abstract contract Executor {
     /**
      * @notice Executes either a delegatecall or a call with provided parameters.
-     * @dev The method doesn't perform any sanity check of the transaction, such as:
-     *      - checking if the contract at `to` address has code or not
+     * @dev This method doesn't perform any sanity check of the transaction, such as:
+     *      - if the contract at `to` address has code or not
      *      It is the responsibility of the caller to perform such checks.
      * @param to Destination address.
      * @param value Ether value.
