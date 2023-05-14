fc6ee41d8483ce3326e64ab40ab385548fecfbf9
nick8319 <zhiltsov.nick@gmail.com>
2023-05-14 20:36:32 +0200

    fix: L2 execution tests for zkSync

--- test/l2/GnosisSafe.Execution.spec.ts
+++ test/l2/GnosisSafe.Execution.spec.ts
@@ -27,13 +27,23 @@ describe("GnosisSafeL2", async () => {
 
     describe("execTransactions", async () => {
 
+        /**
+         * ## In order not to trigger HandlePayment() in GnosisSafe.sol (which uses unsupported send() function)
+         * we don't set gasPrice and refundReceiver
+         * It should be possible to use HandlePayment() after a protocol upgrade (see link2) 
+         * @see https://era.zksync.io/docs/dev/building-on-zksync/contracts/differences-with-ethereum.html#using-call-over-send-or-transfer
+         * @see https://twitter.com/zksync/status/1644459406828924934
+         */
         it('should emit SafeMultiSigTransaction event', async () => {
             const { safe } = await setupTests()
             const tx = buildSafeTransaction({
-                to: user1.address, nonce: await safe.nonce(), operation: 0, gasPrice: 1, safeTxGas: 100000, refundReceiver: user2.address
+                to: user1.address, nonce: await safe.nonce(), operation: 0, 
+                gasPrice: hre.network.zksync ? undefined : 1, 
+                safeTxGas: 100000, 
+                refundReceiver: hre.network.zksync ? undefined : user2.address
             })
 
-            await user1.sendTransaction({ to: safe.address, value: parseEther("1") })
+            await (await user1.sendTransaction({ to: safe.address, value: parseEther("1") })).wait()
             await expect(await hre.ethers.provider.getBalance(safe.address)).to.be.deep.eq(parseEther("1"))
 
             const additionalInfo = ethers.utils.defaultAbiCoder.encode(['uint256', 'address', 'uint256'], [tx.nonce, user1.address, 1])
@@ -65,8 +75,12 @@ describe("GnosisSafeL2", async () => {
             const user2Safe = safe.connect(user2)
             await executeContractCallWithSigners(safe, safe, "enableModule", [user2.address], [user1])
 
+            //Use manual gasLimit because gas estimation fails for this function on zkSync, though transaction executed successfully
             await expect(
-                user2Safe.execTransactionFromModule(mock.address, 0, "0xbaddad", 0)
+                user2Safe.execTransactionFromModule(
+                    mock.address, 0, "0xbaddad", 0, 
+                    { gasLimit: hre.network.zksync ? 500000 : undefined }
+                )
             )
                 .to.emit(safe, "SafeModuleTransaction").withArgs(user2.address, mock.address, 0, "0xbaddad", 0)
                 .to.emit(safe, "ExecutionFromModuleSuccess").withArgs(user2.address)
