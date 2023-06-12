94003dc30a360c5109fb8a2b67b90b27225a54c8
Akshay <akshay.ap95@gmail.com>
2023-06-12 17:51:20 +0200

    EIP-1271: Fix lint issue

--- contracts/handler/CompatibilityFallbackHandler.sol
+++ contracts/handler/CompatibilityFallbackHandler.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "./TokenCallbackHandler.sol";
 import "../interfaces/ISignatureValidator.sol";
 import "../Safe.sol";
-    
+
 /**
  * @title Compatibility Fallback Handler - Provides compatibility between pre 1.3.0 and 1.3.0+ Safe contracts.
  * @author Richard Meissner - @rmeissner
