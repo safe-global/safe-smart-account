0bdfb380128879b00ba79a8c85b4b571bea738ea
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-07-21 13:50:31 +0300

    refactor: adjust guard tests for zksync

--- test/guards/DelegateCallTransactionGuard.spec.ts
+++ test/guards/DelegateCallTransactionGuard.spec.ts
@@ -1,20 +1,20 @@
 import { expect } from "chai";
-import hre, { deployments, ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
 import { AddressZero } from "@ethersproject/constants";
-import { getSafeWithOwners } from "../utils/setup";
+import { getContractFactoryByName, getSafeWithOwners, getWallets } from "../utils/setup";
 import { buildContractCall, executeContractCallWithSigners } from "../../src/utils/execution";
 import { AddressOne } from "../../src/utils/constants";
 
 describe("DelegateCallTransactionGuard", () => {
-    const setupTests = deployments.createFixture(async ({ deployments }) => {
+    const setupTests = hre.deployments.createFixture(async ({ deployments }) => {
         await deployments.fixture();
-        const signers = await ethers.getSigners();
+        const signers = await getWallets();
         const [user1] = signers;
         const safe = await getSafeWithOwners([user1.address]);
-        const guardFactory = await hre.ethers.getContractFactory("DelegateCallTransactionGuard");
+        const guardFactory = await getContractFactoryByName("DelegateCallTransactionGuard");
         const guard = await guardFactory.deploy(AddressZero);
         const guardAddress = await guard.getAddress();
-        await executeContractCallWithSigners(safe, safe, "setGuard", [guardAddress], [user1]);
+        await (await executeContractCallWithSigners(safe, safe, "setGuard", [guardAddress], [user1])).wait();
         return {
             safe,
             guardFactory,
@@ -118,7 +118,7 @@ describe("DelegateCallTransactionGuard", () => {
             } = await setupTests();
             const guard = await guardFactory.deploy(AddressOne);
             const guardAddress = await guard.getAddress();
-            await executeContractCallWithSigners(safe, safe, "setGuard", [guardAddress], [user1]);
+            await (await executeContractCallWithSigners(safe, safe, "setGuard", [guardAddress], [user1])).wait();
 
             expect(await guard.allowedTarget()).to.be.eq(AddressOne);
             const allowedTarget = safe.attach(AddressOne);
--- test/guards/OnlyOwnersGuard.spec.ts
+++ test/guards/OnlyOwnersGuard.spec.ts
@@ -1,19 +1,19 @@
 import { expect } from "chai";
-import hre, { deployments, ethers } from "hardhat";
-import { getMock, getSafeWithOwners } from "../utils/setup";
+import hre, { ethers } from "hardhat";
+import { getContractFactoryByName, getMock, getSafeWithOwners, getWallets } from "../utils/setup";
 import { buildSafeTransaction, executeContractCallWithSigners, executeTxWithSigners } from "../../src/utils/execution";
 
 describe("OnlyOwnersGuard", () => {
-    const setupTests = deployments.createFixture(async ({ deployments }) => {
+    const setupTests = hre.deployments.createFixture(async ({ deployments }) => {
         await deployments.fixture();
-        const signers = await ethers.getSigners();
+        const signers = await getWallets();
         const [user1] = signers;
         const safe = await getSafeWithOwners([user1.address]);
-        const guardFactory = await hre.ethers.getContractFactory("OnlyOwnersGuard");
+        const guardFactory = await getContractFactoryByName("OnlyOwnersGuard");
         const guard = await guardFactory.deploy();
         const guardAddress = await guard.getAddress();
         const mock = await getMock();
-        await executeContractCallWithSigners(safe, safe, "setGuard", [guardAddress], [user1]);
+        await (await executeContractCallWithSigners(safe, safe, "setGuard", [guardAddress], [user1])).wait();
 
         return {
             safe,
