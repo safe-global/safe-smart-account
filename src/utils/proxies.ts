ca587349d1329fbedfd8966f0975d1e04cea7181
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-07-21 13:50:18 +0300

    refactor: adjust prediction of create2 address for zksync

--- src/utils/proxies.ts
+++ src/utils/proxies.ts
@@ -1,10 +1,20 @@
 import { ethers, BigNumberish } from "ethers";
+import hre from "hardhat";
+import * as zk from "zksync-web3";
 import { SafeProxyFactory } from "../../typechain-types";
 
 export const calculateProxyAddress = async (factory: SafeProxyFactory, singleton: string, inititalizer: string, nonce: number | string) => {
+    const salt = ethers.solidityPackedKeccak256(["bytes32", "uint256"], [ethers.solidityPackedKeccak256(["bytes"], [inititalizer]), nonce]);
+
+    if (hre.network.zksync) {
+        const proxyCreationCode = (await hre.artifacts.readArtifact("SafeProxy")).deployedBytecode;
+        const bytecodehash = zk.utils.hashBytecode(proxyCreationCode);
+        const input = new ethers.utils.AbiCoder().encode(["address"], [singleton]);
+        return zk.utils.create2Address(factory.address, bytecodehash, salt, input);
+    }
+
     const factoryAddress = await factory.getAddress();
     const deploymentCode = ethers.solidityPacked(["bytes", "uint256"], [await factory.proxyCreationCode(), singleton]);
-    const salt = ethers.solidityPackedKeccak256(["bytes32", "uint256"], [ethers.solidityPackedKeccak256(["bytes"], [inititalizer]), nonce]);
     return ethers.getCreate2Address(factoryAddress, salt, ethers.keccak256(deploymentCode));
 };
 
@@ -26,11 +36,19 @@ export const calculateChainSpecificProxyAddress = async (
     nonce: number | string,
     chainId: BigNumberish,
 ) => {
-    const factoryAddress = await factory.getAddress();
-    const deploymentCode = ethers.solidityPacked(["bytes", "uint256"], [await factory.proxyCreationCode(), singleton]);
     const salt = ethers.solidityPackedKeccak256(
         ["bytes32", "uint256", "uint256"],
         [ethers.solidityPackedKeccak256(["bytes"], [inititalizer]), nonce, chainId],
     );
+
+    if (hre.network.zksync) {
+        const proxyCreationCode = (await hre.artifacts.readArtifact("SafeProxy")).deployedBytecode;
+        const bytecodehash = zk.utils.hashBytecode(proxyCreationCode);
+        const input = new ethers.utils.AbiCoder().encode(["address"], [singleton]);
+        return zk.utils.create2Address(factory.address, bytecodehash, salt, input);
+    }
+
+    const factoryAddress = await factory.getAddress();
+    const deploymentCode = ethers.solidityPacked(["bytes", "uint256"], [await factory.proxyCreationCode(), singleton]);
     return ethers.getCreate2Address(factoryAddress, salt, ethers.keccak256(deploymentCode));
 };
