0ddb77c90466c89e7b11adaa1ebd65927a68a47a
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-08-23 11:57:22 -0700

    remove settings that are unnecessary

--- certora/scripts/verifySafe.sh
+++ certora/scripts/verifySafe.sh
@@ -10,8 +10,7 @@ certoraRun  certora/harnesses/SafeHarness.sol \
     --verify SafeHarness:certora/specs/Safe.spec \
     --solc solc7.6 \
     --optimistic_loop \
-    --prover_args '-optimisticFallback true -s z3 -copyLoopUnroll 5 -mediumTimeout 3 -depth 20' \
-    --smt_timeout 500 \
+    --prover_args '-optimisticFallback true -s z3 -copyLoopUnroll 5 -mediumTimeout 5 -depth 30' \
     --loop_iter 2 \
     --rule checkSignatures \
     --optimistic_hashing \
