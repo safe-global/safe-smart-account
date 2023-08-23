949a09b4ade7d05585ce1516e665e4b65f5fb1d5
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-08-23 11:53:49 -0700

    update script with more flags to deal with timeout

--- certora/scripts/verifySafe.sh
+++ certora/scripts/verifySafe.sh
@@ -10,8 +10,10 @@ certoraRun  certora/harnesses/SafeHarness.sol \
     --verify SafeHarness:certora/specs/Safe.spec \
     --solc solc7.6 \
     --optimistic_loop \
-    --prover_args '-optimisticFallback true -s z3' \
-    --loop_iter 3 \
+    --prover_args '-optimisticFallback true -s z3 -copyLoopUnroll 5 -mediumTimeout 3 -depth 20' \
+    --smt_timeout 500 \
+    --loop_iter 2 \
+    --rule checkSignatures \
     --optimistic_hashing \
     --hashing_length_bound 352 \
     --rule_sanity \
