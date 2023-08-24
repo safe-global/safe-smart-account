59e9cc660b2f88e5c7b9ab84fe17e79032e9dac4
Mikhail <mveehkim@gmail.com>
2023-08-24 15:19:57 +0200

    split certora depth

--- certora/scripts/verifyOwners.sh
+++ certora/scripts/verifyOwners.sh
@@ -10,7 +10,7 @@ certoraRun  certora/harnesses/SafeHarness.sol \
     --verify SafeHarness:certora/specs/OwnerReach.spec \
     --solc solc7.6 \
     --optimistic_loop \
-    --prover_args '-smt_groundQuantifiers false' \
+    --prover_args '-smt_groundQuantifiers false -depth 20' \
     --loop_iter 3 \
     --optimistic_hashing \
     --hashing_length_bound 352 \
