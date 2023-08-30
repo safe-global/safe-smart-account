protofire/safe-contracts

refactor: adjust Safe.StorageAccessible.spec.ts for zksync

test/utils/contracts.ts
@@ -32,6 +32,38 @@ contract Test {
         revert("Why are you doing this?");
     }
 }`;
-export const killLibContract = async (deployer: Wallet) => {
-    return await deployContract(deployer, killLibSource);
+
+export const killLibSourceZk = `
+contract Test {
+    function expose() public returns (address handler) {
+        bytes32 slot = 0x6c9a6c4a39284e37ed1cf53d337577d14212a4870fb976a4366c693b939918d5;
+        assembly {
+            handler := sload(slot)
+        }
+    }
+
+    function estimate(address to, bytes memory data) public returns (uint256) {
+        uint256 startGas = gasleft();
+        (bool success,) = to.call{ gas: gasleft() }(data);
+        require(success, "Transaction failed");
+        return startGas - gasleft();
+    }
+
+    address singleton;
+    uint256 public value = 0;
+    function updateAndGet() public returns (uint256) {
+        value++;
+        return value;
+    }
+
+    function trever() public returns (address handler) {
+        revert("Why are you doing this?");
+    }
+}`;
+
+export const killLibContract = async (deployer: Wallet, zkSync?: boolean) => {
+    // selfdestruct is not supported by zkSync and compilation cannot be completed, therefore we have to ditch it
+    if (zkSync) return deployContract(deployer, killLibSourceZk);
+
+    return deployContract(deployer, killLibSource);
 };

safe-global/safe-smart-account

test/utils/contracts.ts
@@ -32,6 +32,38 @@ contract Test {
         revert("Why are you doing this?");
     }
 }`;
-export const killLibContract = async (deployer: Signer) => {
-    return await deployContract(deployer, killLibSource);
+
+export const killLibSourceZk = `
+contract Test {
+    function expose() public returns (address handler) {
+        bytes32 slot = 0x6c9a6c4a39284e37ed1cf53d337577d14212a4870fb976a4366c693b939918d5;
+        assembly {
+            handler := sload(slot)
+        }
+    }
+
+    function estimate(address to, bytes memory data) public returns (uint256) {
+        uint256 startGas = gasleft();
+        (bool success,) = to.call{ gas: gasleft() }(data);
+        require(success, "Transaction failed");
+        return startGas - gasleft();
+    }
+
+    address singleton;
+    uint256 public value = 0;
+    function updateAndGet() public returns (uint256) {
+        value++;
+        return value;
+    }
+
+    function trever() public returns (address handler) {
+        revert("Why are you doing this?");
+    }
+}`;
+
+export const killLibContract = async (deployer: Signer, zkSync?: boolean) => {
+    // selfdestruct is not supported by zkSync and compilation cannot be completed, therefore we have to ditch it
+    if (zkSync) return deployContract(deployer, killLibSourceZk);
+
+    return deployContract(deployer, killLibSource);
 };