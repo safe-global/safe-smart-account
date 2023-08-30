protofire/safe-contracts

refactor: adjust handlers tests for zksync

test/utils/setup.ts
@@ -9,71 +9,71 @@ import { getZkContractFactoryByName, zkCompile } from "./zk";
 import { getRandomIntAsString } from "./numbers";
 
 export const defaultTokenCallbackHandlerDeployment = async () => {
-    return await deployments.get("TokenCallbackHandler");
+    return deployments.get("TokenCallbackHandler");
 };
 
 export const defaultTokenCallbackHandlerContract = async () => {
-    return await hre.ethers.getContractFactory("TokenCallbackHandler");
+    return getContractFactoryByName("TokenCallbackHandler");
 };
 
 export const compatFallbackHandlerDeployment = async () => {
-    return await deployments.get("CompatibilityFallbackHandler");
+    return deployments.get("CompatibilityFallbackHandler");
 };
 
 export const compatFallbackHandlerContract = async () => {
-    return await hre.ethers.getContractFactory("CompatibilityFallbackHandler");
+    return getContractFactoryByName("CompatibilityFallbackHandler");
 };
 
 export const getSafeSingleton = async () => {
     const SafeDeployment = await deployments.get(safeContractUnderTest());
-    const Safe = await hre.ethers.getContractFactory(safeContractUnderTest());
+    const Safe = await getContractFactoryByName(safeContractUnderTest());
     return Safe.attach(SafeDeployment.address);
 };
 
 export const getSafeSingletonContract = async () => {
-    const safeSingleton = await hre.ethers.getContractFactory(safeContractUnderTest());
+    const safeSingleton = await getContractFactoryByName(safeContractUnderTest());
 
     return safeSingleton;
 };
 
 export const getFactoryContract = async () => {
-    const factory = await hre.ethers.getContractFactory("SafeProxyFactory");
+    const factory = await getContractFactoryByName("SafeProxyFactory");
 
     return factory;
 };
 
 export const getFactory = async () => {
     const FactoryDeployment = await deployments.get("SafeProxyFactory");
-    const Factory = await hre.ethers.getContractFactory("SafeProxyFactory");
+    const Factory = await getContractFactoryByName("SafeProxyFactory");
     return Factory.attach(FactoryDeployment.address);
 };
 
 export const getSimulateTxAccessor = async () => {
     const SimulateTxAccessorDeployment = await deployments.get("SimulateTxAccessor");
-    const SimulateTxAccessor = await hre.ethers.getContractFactory("SimulateTxAccessor");
+    const SimulateTxAccessor = await getContractFactoryByName("SimulateTxAccessor");
     return SimulateTxAccessor.attach(SimulateTxAccessorDeployment.address);
 };
 
 export const getMultiSend = async () => {
     const MultiSendDeployment = await deployments.get("MultiSend");
-    const MultiSend = await hre.ethers.getContractFactory("MultiSend");
+    const MultiSend = await getContractFactoryByName("MultiSend");
     return MultiSend.attach(MultiSendDeployment.address);
 };
 
 export const getMultiSendCallOnly = async () => {
     const MultiSendDeployment = await deployments.get("MultiSendCallOnly");
-    const MultiSend = await hre.ethers.getContractFactory("MultiSendCallOnly");
+    const MultiSend = await getContractFactoryByName("MultiSendCallOnly");
     return MultiSend.attach(MultiSendDeployment.address);
 };
 
 export const getCreateCall = async () => {
     const CreateCallDeployment = await deployments.get("CreateCall");
-    const CreateCall = await hre.ethers.getContractFactory("CreateCall");
+    const CreateCall = await getContractFactoryByName("CreateCall");
     return CreateCall.attach(CreateCallDeployment.address);
 };
 
 export const migrationContract = async () => {
-    return await hre.ethers.getContractFactory("Migration");
+    return await getContractFactoryByName("Migration");
 };
 
 export const getMock = async () => {
@@ -95,7 +95,7 @@ export const getSafeTemplate = async (saltNumber: string = getRandomIntAsString(
     const factory = await getFactory();
     const template = await factory.callStatic.createProxyWithNonce(singleton.address, "0x", saltNumber);
     await factory.createProxyWithNonce(singleton.address, "0x", saltNumber).then((tx: any) => tx.wait());
-    const Safe = await hre.ethers.getContractFactory(safeContractUnderTest());
+    const Safe = await getContractFactoryByName(safeContractUnderTest());
     return Safe.attach(template);
 };
 

safe-global/safe-smart-account

test/utils/setup.ts
@@ -10,19 +10,19 @@ import { getRandomIntAsString } from "./numbers";
 import { Safe, SafeL2 } from "../../typechain-types";
 
 export const defaultTokenCallbackHandlerDeployment = async () => {
-    return await deployments.get("TokenCallbackHandler");
+    return deployments.get("TokenCallbackHandler");
 };
 
 export const defaultTokenCallbackHandlerContract = async () => {
-    return await hre.ethers.getContractFactory("TokenCallbackHandler");
+    return getContractFactoryByName("TokenCallbackHandler");
 };
 
 export const compatFallbackHandlerDeployment = async () => {
-    return await deployments.get("CompatibilityFallbackHandler");
+    return deployments.get("CompatibilityFallbackHandler");
 };
 
 export const compatFallbackHandlerContract = async () => {
-    return await hre.ethers.getContractFactory("CompatibilityFallbackHandler");
+    return getContractFactoryByName("CompatibilityFallbackHandler");
 };
 
 export const getSafeSingleton = async () => {
@@ -32,13 +32,13 @@ export const getSafeSingleton = async () => {
 };
 
 export const getSafeSingletonContract = async () => {
-    const safeSingleton = await hre.ethers.getContractFactory("Safe");
+    const safeSingleton = await getContractFactoryByName("Safe");
 
     return safeSingleton;
 };
 
 export const getSafeL2SingletonContract = async () => {
-    const safeSingleton = await hre.ethers.getContractFactory("SafeL2");
+    const safeSingleton = await getContractFactoryByName("SafeL2");
 
     return safeSingleton;
 };
@@ -57,7 +57,7 @@ export const getSafeSingletonAt = async (address: string) => {
 };
 
 export const getFactoryContract = async () => {
-    const factory = await hre.ethers.getContractFactory("SafeProxyFactory");
+    const factory = await getContractFactoryByName("SafeProxyFactory");
 
     return factory;
 };
@@ -98,7 +98,7 @@ export const getCreateCall = async () => {
 };
 
 export const migrationContract = async () => {
-    return await hre.ethers.getContractFactory("Migration");
+    return await getContractFactoryByName("Migration");
 };
 
 export const getMock = async () => {
@@ -225,8 +225,8 @@ export const deployContract = async (deployer: Signer, source: string): Promise<
     }
 };
 
-export const getWallets = (): (ethers.Wallet | zk.Wallet)[] => {
-    if (hre.network.name === "hardhat") return waffle.provider.getWallets();
+export const getWallets = async (): (ethers.Signer | zk.Wallet)[] => {
+    if (hre.network.name === "hardhat") return ethers.getSigners();
     if (!hre.network.zksync) throw new Error("You can get wallets only on Hardhat or ZkSyncLocal networks!");
 
     const { accounts } = hre.network.config;