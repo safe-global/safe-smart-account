ebe0ba7447bbe86aa836d8ff44e5c2130fc1c430
Mikhail <mveehkim@gmail.com>
2023-09-01 12:09:43 +0200

    fix linting errors

--- contracts/libraries/Safe130To141Migration.sol
+++ contracts/libraries/Safe130To141Migration.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
+/* solhint-disable one-contract-per-file */
 pragma solidity >=0.7.0 <0.9.0;
 
 import {SafeStorage} from "../libraries/SafeStorage.sol";
@@ -11,6 +12,7 @@ interface ISafe {
  * @title Migration Contract for Safe Upgrade
  * @notice This contract facilitates the migration of a Safe contract from version 1.3.0 to 1.4.1.
  *         The older versions should use built-in upgrade methods.
+ * @dev IMPORTANT: The migration will only work with proxies that store the implementation address in the storage slot 0.
  */
 contract Safe130To141Migration is SafeStorage {
     // Address of this contract
@@ -59,6 +61,7 @@ contract Safe130To141Migration is SafeStorage {
      * @dev This function should only be called via a delegatecall to perform the upgrade.
      */
     function migrateWithFallbackHandler() public {
+        // The `migrate` function will take care of the delegatecall check
         migrate();
 
         ISafe(address(this)).setFallbackHandler(SAFE_141_FALLBACK_HANDLER);
@@ -80,16 +83,45 @@ contract Safe130To141Migration is SafeStorage {
      * @dev This function should only be called via a delegatecall to perform the upgrade.
      */
     function migrateL2WithFallbackHandler() public {
+        // The `migrateL2` function will take care of the delegatecall check
         migrateL2();
 
         ISafe(address(this)).setFallbackHandler(SAFE_141_FALLBACK_HANDLER);
     }
 
+    /**
+     * @notice Checks whether an Ethereum address corresponds to a contract or an externally owned account (EOA).
+     *
+     * @param account The Ethereum address to be checked.
+     *
+     * @return A boolean value indicating whether the address is associated with a contract (true) or an EOA (false).
+     *
+     * @dev This function relies on the `extcodesize` assembly opcode to determine whether an address is a contract.
+     * It may return incorrect results in some edge cases:
+     *
+     * - During the contract deployment process, including the constructor, this function may incorrectly identify the
+     *   contract's own address as an EOA, as the code is not yet deployed.
+     *
+     * - If a contract performs a self-destruct operation (using `selfdestruct`) after deployment, this function may
+     *   incorrectly identify the address as an EOA once the contract is destroyed, as its code will be removed.
+     *
+     * - When interacting with external contracts that use delegatecall or other mechanisms to execute code from
+     *   different contracts, this function may not accurately distinguish between a contract and an EOA, as it only
+     *   checks the code size at the specified address.
+     *
+     * - Contracts that are created using the CREATE2 opcode may not be accurately identified as contracts by this
+     *   function, especially if the code is not deployed until after the creation.
+     *
+     * Developers should use caution when relying on the results of this function for critical decision-making.
+     */
     function isContract(address account) internal view returns (bool) {
         uint256 size;
+        // solhint-disable-next-line no-inline-assembly
         assembly {
             size := extcodesize(account)
         }
+
+        // If the code size is greater than 0, it is a contract; otherwise, it is an EOA.
         return size > 0;
     }
 }
