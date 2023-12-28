f50e2040ddcd88fd6ee2c3b7fd23a48f34574899
Shebin John <admin@remedcu.com>
2023-12-28 18:45:52 +0530

    IEnum added for interface inheritance

---
+++ contracts/interfaces/IEnum.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Enum - Collection of enums used in Safe contracts.
+ */
+interface Enum {
+    enum Operation {
+        Call,
+        DelegateCall
+    }
+}
