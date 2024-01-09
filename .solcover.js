082c7af230e1d30d8f5e4b3d313e27a964bff39c
Shebin John <admin@remedcu.com>
2024-01-09 16:47:33 +0530

    solcover and .env updated

--- .solcover.js
+++ .solcover.js
@@ -12,12 +12,4 @@ module.exports = {
         grep: "@skip-on-coverage", // Find everything with this tag
         invert: true, // Run the grep's inverse set.
     },
-    // Optimization Settings
-    // configureYulOptimizer: true,
-    // solcOptimizerDetails: {
-    //     yul: true,
-    //     yulDetails: {
-    //         optimizerSteps: ""
-    //     },
-    // },
 };
