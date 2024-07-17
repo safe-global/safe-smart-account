0866ea61e57a878804eb1bd6636895feb08b0720
Akshay <akshay.ap95@gmail.com>
2024-07-17 16:54:28 +0200

    [#787] Add SafeMigrations to deploy script

--- src/deploy/deploy_migrations.ts
+++ src/deploy/deploy_migrations.ts
@@ -5,6 +5,9 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre;
     const { deployer } = await getNamedAccounts();
     const { deploy } = deployments;
+    const Safe = await deployments.get("Safe");
+    const SafeL2 = await deployments.get("Safe");
+    const CompatibilityFallbackHandler = await deployments.get("Safe");
 
     await deploy("SafeToL2Migration", {
         from: deployer,
@@ -12,7 +15,15 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         log: true,
         deterministicDeployment: true,
     });
+
+    await deploy("SafeMigration", {
+        from: deployer,
+        args: [Safe.address, SafeL2.address, CompatibilityFallbackHandler.address],
+        log: true,
+        deterministicDeployment: true,
+    });
 };
 
 deploy.tags = ["not-l2-to-l2-migration", "migration"];
+deploy.dependencies = ["singleton", "l2"];
 export default deploy;
