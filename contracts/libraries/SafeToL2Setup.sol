e69f6f0107c4b6488c6946c44fd805a527b17850
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-07-24 12:23:37 +0200

    1.4.1: Backport `SafeToL2Setup` contract (#800)
    A follow up to
    https://github.com/safe-global/safe-smart-account/pull/795 completely
    fixing #781.
    
    This PR migrates:
    - https://github.com/safe-global/safe-smart-account/pull/759
    
    And adjusts the tests for ethers v5
    
    ---------
    
    Co-authored-by: Nicholas Rodrigues Lordello <n@lordello.net>

---
+++ contracts/libraries/SafeToL2Setup.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+
+import {SafeStorage} from "../libraries/SafeStorage.sol";
+
+/**
+ * @title Safe to L2 Setup Contract
+ * @dev This contract expects the singleton to be the {Safe} by default. Even if there are more
+ * {SafeL2} proxies deployed, the average gas cost on L2s is significantly lower, making the
+ * current design more economically efficient overall.
+ * @notice This contract facilitates the deployment of a Safe to the same address on all networks by
+ *         automatically changing the singleton to the L2 version when not on chain ID 1.
+ */
+contract SafeToL2Setup is SafeStorage {
+    /**
+     * @notice Address of the contract.
+     * @dev This is used to ensure that the contract is only ever `DELEGATECALL`-ed.
+     */
+    address public immutable _SELF;
+
+    /**
+     * @notice Event indicating a change of master copy address.
+     * @param singleton New master copy address
+     */
+    event ChangedMasterCopy(address singleton);
+
+    /**
+     * @notice Initializes a new {SafeToL2Setup} instance.
+     */
+    constructor() {
+        _SELF = address(this);
+    }
+
+    /**
+     * @notice Modifier ensure a function is only called via `DELEGATECALL`. Will revert otherwise.
+     */
+    modifier onlyDelegateCall() {
+        require(address(this) != _SELF, "SafeToL2Setup should only be called via delegatecall");
+        _;
+    }
+
+    /**
+     * @notice Modifier to prevent using initialized Safes.
+     */
+    modifier onlyNonceZero() {
+        require(nonce == 0, "Safe must have not executed any tx");
+        _;
+    }
+
+    /**
+     * @notice Modifier to ensure that the specified account is a contract.
+     *
+     */
+    modifier onlyContract(address account) {
+        require(_codeSize(account) != 0, "Account doesn't contain code");
+        _;
+    }
+
+    /**
+     * @notice Setup the Safe with the provided L2 singleton if needed.
+     * @dev This function checks that the chain ID is not 1, and if it isn't updates the singleton
+     *      to the provided L2 singleton.
+     */
+    function setupToL2(address l2Singleton) public onlyDelegateCall onlyNonceZero onlyContract(l2Singleton) {
+        if (_chainId() != 1) {
+            singleton = l2Singleton;
+            emit ChangedMasterCopy(l2Singleton);
+        }
+    }
+
+    /**
+     * @notice Returns the current chain ID.
+     */
+    function _chainId() private view returns (uint256 result) {
+        /* solhint-disable no-inline-assembly */
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := chainid()
+        }
+        /* solhint-enable no-inline-assembly */
+    }
+
+    /**
+     * @notice Returns the code size of the specified account.
+     */
+    function _codeSize(address account) internal view returns (uint256 result) {
+        /* solhint-disable no-inline-assembly */
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := extcodesize(account)
+        }
+        /* solhint-enable no-inline-assembly */
+    }
+}
