0182e25e5945c9ee596a2beeb9897f8046212893
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-07-24 12:23:37 +0200

    1.4.1: Backport `SafeToL2Setup` contract (#800)
    A follow up to
    https://github.com/safe-global/safe-smart-account/pull/795 completely
    fixing #781.
    
    This PR migrates:
    - https://github.com/safe-global/safe-smart-account/pull/759
    
    And adjusts the tests for ethers v5
    
    ---------
    
    Co-authored-by: Nicholas Rodrigues Lordello <n@lordello.net>

--- test/utils/storage.ts
+++ test/utils/storage.ts
@@ -8,6 +8,43 @@ type StateVariable = {
     type: string;
 };
 
+export const EXPECTED_SAFE_STORAGE_LAYOUT: StateVariable[] = [
+    { name: "singleton", slot: "0", offset: 0, type: "t_address" },
+    {
+        name: "modules",
+        slot: "1",
+        offset: 0,
+        type: "t_mapping(t_address,t_address)",
+    },
+    {
+        name: "owners",
+        slot: "2",
+        offset: 0,
+        type: "t_mapping(t_address,t_address)",
+    },
+    { name: "ownerCount", slot: "3", offset: 0, type: "t_uint256" },
+    { name: "threshold", slot: "4", offset: 0, type: "t_uint256" },
+    { name: "nonce", slot: "5", offset: 0, type: "t_uint256" },
+    {
+        name: "_deprecatedDomainSeparator",
+        slot: "6",
+        offset: 0,
+        type: "t_bytes32",
+    },
+    {
+        name: "signedMessages",
+        slot: "7",
+        offset: 0,
+        type: "t_mapping(t_bytes32,t_uint256)",
+    },
+    {
+        name: "approvedHashes",
+        slot: "8",
+        offset: 0,
+        type: "t_mapping(t_address,t_mapping(t_bytes32,t_uint256))",
+    },
+];
+
 export const getContractStorageLayout = async (hre: HardhatRuntimeEnvironment, smartContractName: string) => {
     const { sourceName, contractName } = await hre.artifacts.readArtifact(smartContractName);
 
